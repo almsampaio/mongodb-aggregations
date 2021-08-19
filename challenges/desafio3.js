763a7845a826e093c8e5af80413c99e217db5f7d
Lucas Lara <lucasml@live.com>
2021-08-19 20:30:39 -0300

    Desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: [/crime/i, /horror/i] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: [/english/i, /spanish/i] },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $sort: { ano: -1, notaIMDB: -1, titulo: 1 },
+  },
+]);
