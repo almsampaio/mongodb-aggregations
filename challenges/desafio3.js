a964166ba117722f2bffe9c41f5564859bb36118
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-08-19 18:54:54 -0300

    requisito 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,11 @@
+db.movies.aggregate([{ $match: { "imdb.rating": { $gte: 7 } },
+  genre: { $nin: ["Crime, Horror"] },
+  rated: { $in: ["PG, G"] },
+  laguages: { $all: ["Spanish", "English"] } },
+{ $project: { titulo: "$title",
+  avaliado: "$rated",
+  notaIMDB: "$imdb.rating",
+  votosIMDB: "$imdb.votes",
+  ano: "$year",
+  _id: 0 } },
+{ $sort: { ano: -1, notaIMDB: -1, titulo: 1 } }]);
