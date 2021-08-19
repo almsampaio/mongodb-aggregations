b49949d377a7f8611c8133f76631f44130bc24f0
Marcus C. H. Filho <mchusemann@gmail.com>
2021-08-19 20:30:56 -0300

    feat: desafio 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,12 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } },
+      ],
+    },
+  },
+]);
