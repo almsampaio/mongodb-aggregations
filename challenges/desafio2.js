d858b52c2b743560c7d82c2454d4fb665b1901d0
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-20 16:02:28 -0300

    feat: requisito 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -5,11 +5,14 @@ db.movies.aggregate([
     rated: { $in: ["PG", "G"] },
     languages: { $all: ["English", "Spanish"] },
   } },
-]).project({
-  _id: 0,
-  titulo: "$title",
-  avaliado: "$rated",
-  notaIMDB: "$imdb.rating",
-  votosIMDB: "$imdb.votes",
-  ano: "$year",
-});
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+]);
