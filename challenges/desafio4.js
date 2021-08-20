e405c7e30d89e95dc48cfc44ea68e7d0462ea4ad
biazidioti <bzferreira.bio@gmail.com>
2021-08-20 16:33:27 -0300

    Challenge 4 - ESLint.

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,5 +1,5 @@
 db.movies.aggregate([
-  { $project: 
+  { $project:
     { _id: 0, title_split: { $split: ["$title", " "] } } },
   { $match: { title_split: { $size: 1 } } },
   { $sort: { title_split: 1 } },
