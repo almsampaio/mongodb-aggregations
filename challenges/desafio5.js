700778dbd87a6f854eb6a0be1223621045b55f05
Ana Ventura <anaventura1811@gmail.com>
2021-08-20 14:54:38 -0300

    feat: desafiio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,25 @@
+const favoriteActorsList = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
+db.movies.aggregate([
+  { $match: { $and:
+  [{ cast: { $in: favoriteActorsList } },
+    { countries: { $eq: "USA" } },
+    { "tomatoes.viewer.rating": { $gte: 3 } },
+  ] } }, { $sort: { "tomatoes.viewer.rating": -1, title: -1,
+  } },
+  { $addFields: { num_favs: { $size: { $setIntersection: ["$cast", favoriteActorsList] } },
+  } },
+  { $sort: {
+    num_favs: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
+  } },
+  { $project: { title: 1, _id: 0 } },
+  { $skip: 24 },
+  { $limit: 1 }]);
