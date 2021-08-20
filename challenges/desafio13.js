d47e5130c26421a2ddb7da35d73c38792823fe87
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-20 15:20:52 -0300

    feat: desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,20 @@
+db.trips.aggregate([
+  { $match: { startTime:
+    { $gte: ISODate("2016-03-10T00:00:00Z") },
+  } },
+
+  { $addFields: { minutes: { $divide: [
+    { $subtract: ["$stopTime", "$startTime"] },
+    60 * 1000,
+  ] } } },
+
+  { $group: {
+    _id: null,
+    minutes: { $avg: "$minutes" },
+  } },
+
+  { $project: {
+    _id: 0,
+    duracaoMediaEmMinutos: { $ceil: "$minutes" },
+  } },
+]);
