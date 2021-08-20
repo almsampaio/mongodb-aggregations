c6c4bdbf6405f2f28c04aac4f4803f8f050c8cdb
iago <iago.pferreiravr@gmail.com>
2021-08-20 13:49:21 -0300

    Feat: req5

---
+++ challenges/desafio5.js
@@ -0,0 +1,38 @@
+const actors = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
+db.movies.aggregate([
+  {
+    $match: {
+      cast: { $in: actors },
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: { $size: { $setIntersection: [actors, "$cast"] } },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: { title: 1, _id: 0 },
+  },
+]);
