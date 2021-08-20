893da4a2b3770a80981232a7bc792aa90fc92b46
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-08-20 18:42:57 -0300

    feat: challenge 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,53 @@
+const listFavsActors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        {
+          countries: {
+            $elemMatch: {
+              $eq: "USA",
+            },
+          },
+        },
+        {
+          "tomatoes.viewer.rating": {
+            $gte: 3,
+          },
+        },
+        {
+          cast: { $exists: true },
+        },
+      ],
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: ["$cast", listFavsActors],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
