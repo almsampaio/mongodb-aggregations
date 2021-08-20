295e9c4d6706fc8f934bc3f09066aa337081b020
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-08-20 16:04:04 -0300

    requisitos 5 e 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,7 +1,6 @@
 db.movies.aggregate([
   { $match: { countries: "USA" } },
-  { $match: { "tomatoes.viewer.rating": { $gte: 3 },
-    cast: { $exists: true } } },
+  { $match: { "tomatoes.viewer.rating": { $gte: 3 } } },
   { $project: {
     _id: 0,
     title: 1,
@@ -17,6 +16,6 @@ db.movies.aggregate([
   } },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $project: { title: 1 } },
-  { $limit: 1 },
   { $skip: 24 },
+  { $limit: 1 },
 ]);
