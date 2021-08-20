a0e742ea760f217ee11a160eb25cb734042e9012
Ana Ventura <anaventura1811@gmail.com>
2021-08-20 19:07:45 -0300

    feat: desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  { $match: { $and: [{ birthYear: { $exists: true } }, { birthYear: { $ne: "" } }] } },
+  { $addFields: {
+    convertYear: { $toInt: "$birthYear" },
+  } },
+  { $group: {
+    _id: null,
+    maiorAnoNascimento: { $max: "$convertYear" },
+    menorAnoNascimento: { $min: "$convertYear" },
+  } },
+  { $project: {
+    maiorAnoNascimento: 1,
+    menorAnoNascimento: 1,
+    _id: 0,
+  } },
+]);
+
+// Source:
+// https://docs.mongodb.com/manual/reference/operator/aggregation/toInt/
