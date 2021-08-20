262b0ad13dce251ba363f6a1448c382e4af0462d
guilhermemd <guilherme.md@gmail.com>
2021-08-20 20:48:01 -0300

    feat: :sparkles: req 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,36 @@
+const actors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+db.movies.aggregate([
+  {
+    $match: {
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+    // cast: { $exists: true },
+    cast: { $in: actors },
+  },
+},
+{
+  $addFields: {
+    commonToBoth: { $setIntersection: [ actors, "$cast" ] },
+  },
+},
+{
+  $addFields: {
+    num_favs: { $size: "$commonToBoth" },
+  },
+},
+{
+  $sort: {
+    num_favs: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
+  },
+},
+{
+  $project: {
+    title: 1,
+    _id: 0,
+  }
+},
+{ $skip: 24 },
+{ $limit: 1},
+]);
