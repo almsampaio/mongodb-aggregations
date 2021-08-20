736319d1e00a9c212022fbe437de5304537c8fcd
lucas-chave <lcchavesr150@gmail.com>
2021-08-20 15:36:57 -0300

    challenge 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,10 @@
+// challenge 1;
+
+db.movies.aggregate(
+  [
+    { $match: { $and: [
+      { "imdb.rating": { $gte: 7 } },
+      { genres: { $nin: ["Crime", "Horror"] } },
+      { rated: { $in: ["PG", "G"] } },
+      { languages: { $in: ["English", "Spanish"] } }
+    ] } }]);
