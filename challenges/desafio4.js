cfd2d4210872fc37a88d76765b143a7936f3d36d
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-20 16:19:51 -0300

    Req 4 - retorna filmes com 1 palavra no titulo ordenados

---
+++ challenges/desafio4.js
@@ -0,0 +1,20 @@
+db.movies.aggregate([
+  {
+    $project: {
+      title_split: { $split: ["$title", " "] },
+      _id: 0,
+    },
+  },
+  {
+    $match: {
+      title_split: {
+        $size: 1,
+      },
+    },
+  },
+  {
+    $sort: {
+      "title_split.0": 1,
+    },
+  },
+]);
