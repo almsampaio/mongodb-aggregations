e7b0152b8ced4e31c3997e909bf1bddd016a4b30
carlos25xrm <carlos25xrm@gmail.com>
2021-08-20 17:51:39 -0300

    feat: desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,20 @@
+db.movies.aggregate([
+  { $addFields:
+    { title_split: { $split: ["$title", " "] }, },
+  },
+  { $match:
+    {
+      title_split: { $size: 1 },
+    },
+  },
+  { $project:
+    {
+      title_split: 1,
+      _id: 0,
+    },
+  },
+  { $sort: { title_split: 1 } },
+]);
+
+// https://docs.mongodb.com/manual/reference/operator/aggregation/split/
+// Usar o $split para dividir e verificar qual array possui somente 1 posição
