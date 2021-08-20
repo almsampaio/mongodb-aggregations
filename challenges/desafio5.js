f28a234e408875bcabc3910576a53dd91dbcac2b
guilhermemd <guilherme.md@gmail.com>
2021-08-20 20:49:57 -0300

    feat: :sparkles: req 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,36 +1,47 @@
 const actors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
-db.movies.aggregate([
-  {
+db.movies.aggregate([{
     $match: {
-    countries: "USA",
-    "tomatoes.viewer.rating": { $gte: 3 },
-    // cast: { $exists: true },
-    cast: { $in: actors },
+      countries: "USA",
+      "tomatoes.viewer.rating": {
+        $gte: 3
+      },
+      // cast: { $exists: true },
+      cast: {
+        $in: actors
+      },
+    },
   },
-},
-{
-  $addFields: {
-    commonToBoth: { $setIntersection: [ actors, "$cast" ] },
+  {
+    $addFields: {
+      commonToBoth: {
+        $setIntersection: [actors, "$cast"]
+      },
+    },
   },
-},
-{
-  $addFields: {
-    num_favs: { $size: "$commonToBoth" },
+  {
+    $addFields: {
+      num_favs: {
+        $size: "$commonToBoth"
+      },
+    },
   },
-},
-{
-  $sort: {
-    num_favs: -1,
-    "tomatoes.viewer.rating": -1,
-    title: -1,
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
   },
-},
-{
-  $project: {
-    title: 1,
-    _id: 0,
-  }
-},
-{ $skip: 24 },
-{ $limit: 1},
+  {
+    $project: {
+      title: 1,
+      _id: 0,
+    }
+  },
+  {
+    $skip: 24
+  },
+  {
+    $limit: 1
+  },
 ]);
