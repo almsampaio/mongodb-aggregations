645c2ab3888fdfe5da159ffa3a7c65cde959e770
carlos25xrm <carlos25xrm@gmail.com>
2021-08-20 17:28:57 -0300

    fix: desafio 2 e 3 - Retirando espaço entre as aspas do votosIMDB

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -12,7 +12,7 @@ db.movies.aggregate([
       titulo: "$title",
       avaliado: "$rated",
       notaIMDB: "$imdb.rating",
-      votosIMDB: " $imdb.votes",
+      votosIMDB: "$imdb.votes",
       ano: "$year",
       _id: 0,
     },
