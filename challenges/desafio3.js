749f9d27ea3de06b17607a6e91dd803de55650cc
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-20 16:06:16 -0300

    fix: requisito 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -5,15 +5,21 @@ db.movies.aggregate([
     rated: { $in: ["PG", "G"] },
     languages: { $all: ["English", "Spanish"] },
   } },
-]).project({
-  _id: 0,
-  titulo: "$title",
-  avaliado: "$rated",
-  notaIMDB: "$imdb.rating",
-  votosIMDB: "$imdb.votes",
-  ano: "$year",
-}).sort({
-  ano: -1,
-  notaIMDB: -1,
-  titulo: 1,
-});
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $sort: {
+      ano: -1,
+      notaIMDB: -1,
+      titulo: 1,
+    },
+  },
+]);
