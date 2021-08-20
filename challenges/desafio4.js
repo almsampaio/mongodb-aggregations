c36152b20b555705548d33892a8fd8a60c9ffeda
lucas-chave <lcchavesr150@gmail.com>
2021-08-20 18:13:21 -0300

    challenge 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,8 @@
+// challenge 4;
+
+db.movies.aggregate(
+  [
+    { $project: { title_split: { $split: ["$title", " "] }, _id: 0 } },
+    { $match: { title_split: { $size: 1 } } },
+  ],
+).sort({ title_split: 1 });
