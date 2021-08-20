e457a73424c9902f47be747e95e9da586e714367
Eduardo Seije <eduardoseije@gmail.com>
2021-08-20 15:56:18 -0300

    Desafio 04 - ajuste lint - remoção espaçõs colchetes

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -3,7 +3,7 @@ db.movies.aggregate(
     {
       $project: {
         _id: 0,
-        title_split: { $split: [ "$title", " " ] },
+        title_split: { $split: ["$title", " "] },
       },
     },
     {
