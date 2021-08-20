0a25a8304bd914951bf2b7e12a344ca62eee7ddf
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-08-20 14:19:30 -0400

    feat: desafio 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,23 @@
+// use("aggregations");
+db.movies.aggregate([
+  {
+    $match:
+      {
+        $or: [{ rated: { $in: ["G", "PG"] } }],
+        $and: [{ languages: "English" }, { languages: "Spanish" }],
+        $nor: [{ genres: { $in: ["Crime", "Horror"] } }],
+        "imdb.rating": { $gte: 7 },
+      },
+  },
+  {
+    $project:
+      {
+        _id: 0,
+        titulo: "$title",
+        avaliado: "$rated",
+        notaIMDB: "$imdb.rating",
+        votosIMDB: "$imdb.votes",
+        ano: "$year",
+      },
+  },
+]);
