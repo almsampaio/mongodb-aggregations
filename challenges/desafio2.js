7970bcb3c73954e291df968b740cc67fbb4edea0
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-08-20 14:43:06 -0300

    requisitos 2 e 3 corrigidos

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,7 +1,7 @@
 db.movies.aggregate([{ $match: { "imdb.rating": { $gte: 7 } },
   genre: { $nin: ["Crime, Horror"] },
   rated: { $in: ["PG, G"] },
-  laguages: { $all: ["Spanish", "English"] } },
+  languages: { $all: ["Spanish", "English"] } },
 { $project: { titulo: "$title",
   avaliado: "$rated",
   notaIMDB: "$imdb.rating",
