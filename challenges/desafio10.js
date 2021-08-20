f43ecde2688b996683506eb834a5e7898ad256cf
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-20 15:19:50 -0300

    feat: desafio 10 and 11

---
+++ challenges/desafio10.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  { $addFields: { time: { $divide: [
+    { $subtract: ["$stopTime", "$startTime"] },
+    60 * 60 * 1000,
+  ] } } },
+
+  { $group: {
+    _id: "$usertype",
+    duracaoMedia: { $avg: "$time" },
+  } },
+
+  { $project: {
+    _id: 0,
+    tipo: "$_id",
+    duracaoMedia: { $round: ["$duracaoMedia", 2] },
+  } },
+
+  { $sort: { duracaoMedia: 1 } },
+]);
