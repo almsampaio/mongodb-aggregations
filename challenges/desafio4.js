4c09b6c83e2b2104360ace88efb1d1b512cfec87
luanlsr <luanfswd@gmail.com>
2021-08-20 15:17:56 -0300

    feat: :sparkles: finished req 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,1 +1,12 @@
 // Desafio 4
+db.movies.aggregate([
+  {
+    $match: {
+      $expr: { //$expr para verificar a igualdade do split com o size feita com ajuda do PR do Murilo
+        $eq: [{ $size: { $split: ["$title", " "] } }, 1],
+      },
+    },
+  },
+  { $sort: { title: 1 } },
+  { $project: { _id: 0, title_split: { $split: ["$title", " "] } } },
+]);
