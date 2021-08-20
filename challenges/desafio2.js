165cd08cd2ecae89482d242c590a667dd926f31b
w-cypriano <profwellington17@gmail.com>
2021-08-20 19:51:05 -0300

    iniciando o projeto com desafio2

---
+++ challenges/desafio2.js
@@ -0,0 +1,27 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and:
+      [
+        { "imdb.rating": { $gte: 7 } },
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } },
+      ],
+    },
+  },
+  {
+    $addFields: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $project: {
+      titulo: 1, avaliado: 1, notaIMDB: 1, votosIMDB: 1, ano: 1, _id: 0,
+    },
+  },
+]);
