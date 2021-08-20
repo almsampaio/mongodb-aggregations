7e4df93abc3c7cda64f8c1035b37324498a2d576
rogerio-lambert <rogerio.cl@gmail.com>
2021-08-20 17:43:55 -0300

    correçoes nos desafios 1, 2 e 3

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,7 +1,7 @@
 db.movies.aggregate([
   { $match: {
     "imdb.rating": { $gte: 7 },
-    genres: { $elemMatch: { $nin: ["Crime", "Horror"] } },
+    genres: { $nin: ["Crime", "Horror"] },
     rated: { $in: ["PG", "G"] },
     languages: { $all: ["English", "Spanish"] },
   } },
