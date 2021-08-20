c059d610f601354f71489f4d7c2b9084eec38afa
guilhermemd <guilherme.md@gmail.com>
2021-08-20 15:10:17 -0300

    fix: :bug: req 01

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,5 +1,6 @@
 // Desafio 1
-// Ajude a Trybe a escolher um filme para a próxima noite! Baseado em uma pesquisa, decidimos que os filmes em potencial devem atender alguns critérios, vejamos:
+// Ajude a Trybe a escolher um filme para a próxima noite! Baseado em uma pesquisa, 
+// decidimos que os filmes em potencial devem atender alguns critérios, vejamos:
 
 // Retorne todos os filmes que satisfaça, através de uma pipeline, as condições abaixo
 // imdb.rating deve ser maior ou igual a 7;
@@ -8,19 +9,13 @@
 // languages contém English e Spanish.
 // Utilize a coleção movies.
 // Sua query deve retornar 41 documentos.
-db.movies.aggregate([{
-  $match: {
-    "imdb.rating": {
-      $gte: 7
-    },
-    genres: {
-      $nin: ["Crime", "Horror"]
-    },
-    rated: {
-      $in: ["PG", "G"]
-    },
-    languages: {
-      $all: ["English", "Spanish"]
-    },
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    }
   }
-}]);
+]);
