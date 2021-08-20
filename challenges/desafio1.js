c61dd86d9cb56eace76c6995b70eca632e8a7df6
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-08-20 14:33:17 -0300

    Feat: desafio 3

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,10 +1,10 @@
 db.movies.aggregate([
   { $match:
-    { 
-    "imdb.rating": { $gte: 7 },
-    genres: { $nin: ["Crime", "Horror"] },
-    rated: { $in: ["PG", "G"] },
-    $and: [{ languages: "English"}, { languages: "Spanish" }]
-    }
-  }
+    {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      $and: [{ languages: "English" }, { languages: "Spanish" }],
+    },
+  },
 ]);
