4f545107adfdc875e96e824b7394bf580215daa9
luanlsr <luanfswd@gmail.com>
2021-08-19 22:14:12 -0300

    feat: :sparkles: finished req 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,1 +1,37 @@
 // Desafio 3
+use("aggregations");
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": {
+        $gte: 7,
+      },
+      genres: {
+        $nin: ["Horror", "Crime"],
+      },
+      rated: {
+        $in: ["PG", "G"],
+      },
+      languages: {
+        $all: ["English", "Spanish"],
+      },
+    },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+      _id: 0,
+    },
+  },
+  {
+    $sort: {
+      ano: 1,
+      notaIMDB: 1,
+      titulo: -1,
+    },
+  },
+]);
