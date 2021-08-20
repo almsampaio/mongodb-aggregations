18f77ace77d708c6500da39d627a0d1ba6462b49
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-20 19:08:45 -0300

    Desafio 3 concluído

---
+++ challenges/desafio3.js
@@ -0,0 +1,11 @@
+db.movies.aggregate([{ $match: { "imdb.rating": { $gte: 7 } } },
+  { $match: { $nor: [{ genres: "Crime" }, { genres: "Horror" }] } },
+  { $match: { rated: { $in: ["PG", "G"] } } },
+  { $match: { $and: [{ languages: "English" }, { languages: "Spanish" }] } },
+  { $project: { _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year" } },
+  { $sort: { ano: -1, notaIMDB: -1, titulo: 1 } }]);
