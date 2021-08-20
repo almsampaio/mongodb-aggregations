211e1dc820c1de9aa9f81a470ae3642fba832583
RenzoSev <sevilharenzo@gmail.com>
2021-08-20 12:16:54 -0300

    fix: desafio12

---
+++ challenges/desafio12.js
@@ -0,0 +1,31 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        diaSemana: { $dayOfWeek: "$startTime" },
+        nomeEstacao: "$startStationName",
+      },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $match: {
+      "_id.diaSemana": 5,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.nomeEstacao",
+      total: "$total",
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
