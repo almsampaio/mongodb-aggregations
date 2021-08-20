cba2b74a27a78f46e40a61cc07ae2a7ccb875430
Bruno Mendes <mendessbruno@gmail.com>
2021-08-20 15:58:33 -0300

    resolvido desafio 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,15 @@
+db.movies.aggregate(
+  [
+    {
+      $match: {
+        $and: [
+          { "imdb.rating": { $gte: 7 } },
+          { genres: { $nin: ["Crime", "Horror"] } },
+          { rated: { $in: ["PG", "G"] } },
+          { languages: { $elemMatch: { $eq: "English" } } },
+          { languages: { $elemMatch: { $eq: "Spanish" } } },
+        ],
+      },
+    },
+  ],
+);
