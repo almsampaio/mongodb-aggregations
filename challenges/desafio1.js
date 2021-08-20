9e0b22cd7d70ff6f836dd19c1cf9c9ad792e08cd
Gabriel <g.pereira.a.m@gmail.com>
2021-08-20 20:39:08 -0300

    feat(Req 1) - :sparkle: Desafio 1 feito

---
+++ challenges/desafio1.js
@@ -0,0 +1,16 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": {
+        $gte: 7,
+      },
+      genres: {
+        $not: { $in: ["Crime", "Horror"] },
+      },
+      rated: { $in: ["PG", "G"] },
+      languages: {
+        $all: ["English", "Spanish"],
+      },
+    },
+  },
+]);
