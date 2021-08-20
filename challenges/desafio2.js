b456d0a558b0e9563106d7c0b1b33198715c5e05
maricegalla <maricegalla@gmail.com>
2021-08-20 17:15:45 -0300

    Desafio 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,10 @@
+db.movies.aggregate([
+  { $match: {
+    "imdb.rating": { $gte: 7 },
+    $and: [{ languages: { $in: ["English"] } }, { languages: { $in: ["Spanish"] } }],
+    $or: [{ rated: "G" }, { rated: "PG" }],
+    $nor: [{ genres: "Crime" }, { genres: "Horror" }],
+
+  } },
+  { $project: { titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year", _id: 0 } },
+]);
