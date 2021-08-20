3da46f1d7101f294a2830a63befefc0c18aba226
carlos25xrm <carlos25xrm@gmail.com>
2021-08-20 16:18:06 -0300

    fix: desafio 1 - erros do eslint

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,14 +1,14 @@
 db.movies.aggregate([
-    { $match:
+  { $match:
         {
-            "imdb.rating": { $gte: 7 },
-            genres: { $nin: ["Crime", "Horror"] },
-            rated: { $in: ["PG", "G"] },
-            languages: { $all: ["English", "Spanish"] }
-        }
-    }
+          "imdb.rating": { $gte: 7 },
+          genres: { $nin: ["Crime", "Horror"] },
+          rated: { $in: ["PG", "G"] },
+          languages: { $all: ["English", "Spanish"] },
+        },
+  },
 ]);
 
 // https://docs.mongodb.com/manual/reference/operator/query/all/
 // The $all operator selects the documents where the value of
-// a field is an array that contains all the specified elements. 
+// a field is an array that contains all the specified elements.
