ae02186c1611579a732e7f3145c610f572949dfa
Orlando Flores <orlando_flores@id.uff.br>
2021-08-20 17:56:40 -0300

    feat: desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,32 @@
+db.air_routes.aggregate([
+  {
+    $match: {
+      airplane: { $in: ["747", "380"] },
+    },
+  },
+  {
+    $lookup: {
+      from: "air_alliances",
+      localField: "airline.name",
+      foreignField: "airlines",
+      as: "air_routes",
+    },
+  },
+  {
+    $unwind: "$air_routes",
+  },
+  {
+    $group: {
+      _id: "$air_routes.name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
