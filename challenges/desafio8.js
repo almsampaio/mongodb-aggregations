e7edd98012a6bc2cbd1a54101f8d2e9173659dde
Jodiel Briesemeister <jodielll29@gmail.com>
2021-08-20 19:22:53 -0300

    feat: req08, 09

---
+++ challenges/desafio8.js
@@ -0,0 +1,26 @@
+db.air_routes.aggregate([
+  {
+    $match: {
+      airplane: { $in: ["747", "380"] },
+    },
+  },
+  {
+    $lookup: {
+      from: "air_alliances",
+      localField: "airline.name",
+      foreignField: "airlines",
+      as: "string",
+    },
+  },
+  { $unwind: "$string" },
+  {
+    $group: {
+      _id: "$string.name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { totalRotas: -1 },
+  },
+  { $limit: 1 },
+]);
