33bc5d7679f9cad5511d44034affc17367a45a92
Marina Alane <marina.alane@outlook.com>
2021-08-20 17:07:36 -0300

    desafio 1,2 e 3

---
+++ challenges/desafio1.js
@@ -0,0 +1,17 @@
+// Retorne todos os filmes que satisfaça, através de uma pipeline, as condições abaixo
+// imdb.rating deve ser maior ou igual a 7;
+// genres não deve conter Crime ou Horror;
+// rated deve ser igual a PG ou G;
+// languages contém English e Spanish.
+// Utilize a coleção movies.
+// Sua query deve retornar 41 documentos.
+
+db.movies.aggregate([{ $match:
+  { $and: [
+    { "imdb.rating": { $gte: 7 } },
+    { genres: { $ne: "Crime" } },
+    { genres: { $ne: "Horror" } },
+    { $or: [{ rated: { $eq: "G" } }, { rated: { $eq: "PG" } }] },
+    { $and: [{ languages: { $eq: "English" } }, { languages: { $eq: "Spanish" } }] },
+  ] } }, { $limit: 41 }]);
+  
