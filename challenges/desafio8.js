2ce4c3314d69e7daec831bde10ea0029cd34e6d1
Lucas Lara <lucasml@live.com>
2021-08-20 14:56:16 -0300

    Desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,16 @@
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "routes",
+    },
+  },
+  { $unwind: "$routes" },
+  { $match: { "routes.airplane": { $in: ["747", "380"] } } },
+  { $group: { _id: "$name", totalRotas: { $sum: 1 } } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+]);
