343778e5fc63939d202b77574acc4401d14bd646
Hudson Carlos <hc88806@gmail.com>
2021-08-20 19:54:11 -0400

    challenge one tow

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,11 +2,11 @@ db.movies.aggregate([{ $match: { "imdb.rating": { $gte: 7 },
   genres: { $nin: ["Horror", "Crime"] },
   rated: { $in: ["PG", "G"] },
   languages: { $all: ["English", "Spanish"] } } },
-  { $project: {
-    titulo: "$title",
-    avaliado: "$rated",
-    notaIMDB: "$imdb.rating",
-    votosIMDB: "$imdb.votes",
-    ano: "$year",
-    _id: 0,
-  } }]);
+{ $project: {
+  titulo: "$title",
+  avaliado: "$rated",
+  notaIMDB: "$imdb.rating",
+  votosIMDB: "$imdb.votes",
+  ano: "$year",
+  _id: 0,
+} }]);
