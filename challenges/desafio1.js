3f232d48bd234000e7733f37a55c2c3c9bfe95df
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-20 19:02:13 +0000

    refactor: challenge 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,6 +1,8 @@
 db.movies.aggregate([
-  { $match: { "imdb.rating": { $gte: 7 } } },
-  { $match: { genres: { $nin: ["Crime", "Horror"] } } },
-  { $match: { rated: { $in: ["PG", "G"] } } },
-  { $match: { languages: { $all: ["English", "Spanish"] } } },
+  { $match: {
+    "imdb.rating": { $gte: 7 },
+    genres: { $nin: ["Crime", "Horror"] },
+    rated: { $in: ["PG", "G"] },
+    languages: { $all: ["English", "Spanish"] },
+  } },
 ]);
