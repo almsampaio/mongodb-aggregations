56185fe1505bd69ed2b71be76a957ce1a0df67d1
Giovanni Maldonado <giovannitadeu08@gmail.com>
2021-08-20 17:47:31 -0300

    feat: desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,27 @@
+db.movies.aggregate([
+  {
+    $match: { languages: { $all: ["English"] } },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $sort: {
+      numeroFilmes: -1,
+      _id: -1,
+    },
+  },
+  {
+    $project: {
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+]);
