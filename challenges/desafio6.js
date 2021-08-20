313b893ba62388abc22c36ca92cd1d2832397b63
MarcioFujita <hanker78@gmail.com>
2021-08-20 16:31:46 -0300

    Requisitos 4 ao 6 V2

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -21,7 +21,7 @@ db.movies.aggregate([
       maior_rating: 1,
       menor_rating: 1,
       media_rating: { $round: ["$media_rating", 1] },
-      desvio_padrao: { $round: ["$desvio_padrao", 1]},
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
   },
 ]);
