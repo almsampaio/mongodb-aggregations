e66c94ffd7ba397a8eada6ddcef86359fb71a357
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-20 19:11:34 -0300

    Feat: (req-2) return only the title, rated, imdb.rating, imdb.votes and year fields, modifying their names to title, rated, noteIMDB, votesIMDB and year, respectively

---
+++ challenges/desafio2.js
@@ -0,0 +1,34 @@
+// use("aggregations");
+// db.movies.find();
+
+db.movies.aggregate(
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      // title:1,
+      // rated:1,
+      // "imdb.rating":1,
+      // "imdb.votes":1,
+      // year:1,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  // {
+  //   $group:{
+  //     _id:null,
+  //     count:{$sum:1}
+  //   }
+  // }
+);
