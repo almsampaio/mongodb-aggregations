a8c8d04c0b085ef45185e2181f9331b9fb30d42a
MarcioFujita <hanker78@gmail.com>
2021-08-20 15:31:36 -0300

    Requisitos 1 ao 3 V2

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,7 +2,7 @@ db.movies.aggregate([
   {
     $match: {
       "imdb.rating": { $gte: 7 },
-      genres: { $nin: ["Crime", "Horror"]},
+      genres: { $nin: ["Crime", "Horror"] },
       rated: { $in: ["PG", "G"] },
       languages: { $all: ["English", "Spanish"] },
     },
