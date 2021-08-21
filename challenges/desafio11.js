cb4f7eae9d381c2b75065d27d2e49ae462efb7a4
luanlsr <luanfswd@gmail.com>
2021-08-21 14:49:02 -0300

    feat: :sparkles: finished req 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,1 +1,25 @@
 // Desafio 11
+db.trips.aggregate([
+  {
+    $group: {
+      _id:  { $dayOfWeek: "$startTime"},
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    }
+  },
+  {
+    $limit: 1
+  },
+]);
