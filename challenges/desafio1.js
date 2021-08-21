35b3b70e89b37942f5b4c254c109c48e14a089f9
Felipe Muller <felipeavmuller@gmail.com>
2021-08-21 00:37:57 -0300

    Fix: desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,7 +2,7 @@ db.movies.aggregate([
   {
     $match: {
       "imdb.rating": { $gte: 7 },
-      genre: { $nin: ["Crime", "Horror"] },
+      genres: { $nin: ["Crime", "Horror"] },
       rated: { $in: ["PG", "G"] },
       languages: { $all: ["English", "Spanish"] },
     },
