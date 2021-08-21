036ad2b0f0332e9803a9af5f3e1c3fee5b3f4fdf
Robertson <robertson.nunes@gmail.com>
2021-08-21 17:17:43 -0300

    Requisito 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,22 @@
+/* Ajude a Trybe a escolher um filme para a próxima noite! Baseado em uma pesquisa,
+decidimos que os filmes em potencial devem atender alguns critérios, vejamos:
+Retorne todos os filmes que satisfaça, através de uma pipeline, as condições abaixo
+imdb.rating deve ser maior ou igual a 7;
+genres não deve conter Crime ou Horror;
+rated deve ser igual a PG ou G;
+languages contém English e Spanish.
+Utilize a coleção movies.
+Sua query deve retornar 41 documentos. */
+
+db.movies.aggregate(
+  [
+    {
+      $match: {
+        $and:
+        [
+          {
+            "imdb.rating": { $gte: 7 } },
+          { genres: { $nin: ["Crime", "Horror"] } },
+          { rated: { $in: ["PG", "G"] } },
+          { languages: { $all: ["English", "Spanish"] } }] } }],
+);
