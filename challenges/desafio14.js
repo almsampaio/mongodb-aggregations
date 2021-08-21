e7505c7548aa7de61fd130e73879fbb9a2a24a99
maricegalla <maricegalla@gmail.com>
2021-08-21 08:56:32 -0300

    Desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,7 @@
+db.trips.aggregate([
+  { $addFields: { duration: { $round: [{ $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000] }, 2] } } },
+  { $group: { _id: "$bikeid", duracaoMedia: { $avg: "$duration" } } },
+  { $project: { bikeId: "$_id", duracaoMedia: { $ceil: "$duracaoMedia" }, _id: 0 } },
+  { $sort: { duracaoMedia: -1 } },
+  { $limit: 5 },
+]);
