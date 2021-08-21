1da4ffbe9e67c7602e7b7b81553b624b38aafaf1
hheloisa <hhackenhaar@gmail.com>
2021-08-21 15:19:27 -0300

    Finaliza requisito 8 - Liste todas as parcerias da coleção air_alliances, que voam rotas com um Boing 747 ou um Airbus A380.

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,5 +1,5 @@
 db.air_routes.aggregate([
-  { $match: { airplane: { $in: [747, 380] } } },
+  { $match: { airplane: { $in: ["747", "380"] } } },
   { $lookup: {
     from: "air_alliances", // coleção externa
     let: { airlineNames: "$airline.name" }, // variavel armazena dado local 
@@ -14,22 +14,23 @@ db.air_routes.aggregate([
               }, // ^compara o nome descompactado com o nome local
           },
         },
-        { $project:
+        { $project: // o que fara parte do campo alliances_routes
           {
             _id: 1,
             name: 1,
           },
         },
       ],
-    as: "alliances_routes",
+    as: "alliances_routes", // atribui nome ao novo campo formado
+    // que é um array de objetos(chave/valor)
   } },
-  { $unwind: "$alliances_routes" },
+  { $unwind: "$alliances_routes" }, // desestrutura o array
   { $group:
     {
-      _id: "$alliances_routes.name",
-      totalRotas: { $sum: 1 },
+      _id: "$alliances_routes.name", // agrupa pela pripriedade nome
+      totalRotas: { $sum: 1 }, // soma mais um a cada repetição
     },
   },
-  { sort: { totalRotas: -1 } },
-  { limit: 1 },
+  { $sort: { totalRotas: -1 } }, // ordena do maior pro menor
+  { $limit: 1 }, // limita a um resultado
 ]).pretty();
