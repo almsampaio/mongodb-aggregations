b9bed99536528866f90e6015ac3210c36869b3aa
Robertson <robertson.nunes@gmail.com>
2021-08-21 18:14:14 -0300

    Requisito 7

---
+++ challenges/desafio6.js
@@ -0,0 +1,8 @@
+/* Vamos explorar mais operadores aritméticos!
+
+Considerando todos os filmes que ganharam o Oscar pelo menos uma vez, calcule o maior valor,
+menor valor, média e o desvio padrão das avaliações (informação do campo imdb.rating no banco)
+Para a média e o desvio padrão arredonde os valores para uma casa decimal utilizando o $round.
+Dica: todos os filmes na coleção, que já ganharam um Oscar (informação do campo awards no banco),
+começam com uma sequência de string parecida com essas abaixo, portanto $regex é um operador
+bem-vindo */
