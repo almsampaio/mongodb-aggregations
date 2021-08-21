2ed57517ff5988b488b02ade3ec8a093ca19f2fb
Robertson <robertson.nunes@gmail.com>
2021-08-21 18:35:23 -0300

    Requisito 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -6,3 +6,5 @@ Para a média e o desvio padrão arredonde os valores para uma casa decimal util
 Dica: todos os filmes na coleção, que já ganharam um Oscar (informação do campo awards no banco),
 começam com uma sequência de string parecida com essas abaixo, portanto $regex é um operador
 bem-vindo */
+
+db.movies.aggregate([{ $match: { awards: { $regex: /^Won\s\d+\sOscar?/ } } }, { $group: { _id: null, maior_rating: { $max: "$imdb.rating" }, menor_rating: { $min: "$imdb.rating" }, media_rating: { $avg: "$imdb.rating" }, desvio_padrao: { $stdDevSamp: "$imdb.rating" } } }, { $project: { _id: 0, maior_rating: 1, menor_rating: 1, media_rating: { $round: ["$media_rating", 1] }, desvio_padrao: { $round: ["$desvio_padrao", 1] } } }]);
