5f4623884967d2c094704699b2b30738df72be26
Maria Baeta <m.martinsdg@gmail.com>
2021-08-20 22:01:03 -0300

    fix: :bug: Correção requisito 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -3,7 +3,7 @@ db.movies.aggregate([{
     "imdb.rating": { $gte: 7 },
     genres: { $nin: ["Crime", "Horror"] },
     rated: { $in: ["PG", "G"] },
-    languages: ["English", "Spanish"],
+    languages: { $all: ["English", "Spanish"] },
   },
 }, {
   $project: {
