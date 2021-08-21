1d61d011a1ea3fc4db7c897f40e22b7a9679e545
maricegalla <maricegalla@gmail.com>
2021-08-21 07:57:34 -0300

    Desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,4 +2,5 @@ db.trips.aggregate([
   { $addFields: { duration: { $round: [{ $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] }, 2] } } },
   { $group: { _id: "$usertype", duracaoMedia: { $avg: "$duration" } } },
   { $project: { tipo: "$_id", duracaoMedia: { $round: ["$duracaoMedia", 2] }, _id: 0 } },
+  { $sort: { tipo: -1 } },
 ]);
