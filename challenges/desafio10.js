a0e03f25dec254eb63fc6227a813e0f6c5cae4e6
luanlsr <luanfswd@gmail.com>
2021-08-21 14:03:11 -0300

    feat: :sparkles: finished req 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,25 @@
 // Desafio 10
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3.6e6] } },
+    },
+    // O resultado da subtração retorna um valor em milisegundos,
+    // logo é necessário dividir o valor por 3.6e6 que é o equivalente a 1 hora
+    // e assim conseguir o valor em horas para extrair a média no formato pedido no desafio.
+    // lógica da divisão extraída do PR #4 do Murilo Gonçalves
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: 1,
+    },
+  },
+]);
