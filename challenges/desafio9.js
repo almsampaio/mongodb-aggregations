f2ddb2200b0c445b0c79e5f6c4b5c0f88d505794
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-21 18:24:38 -0300

    Req 9 - Retorna menor e maior ano de nasc da colecao trips

---
+++ challenges/desafio9.js
@@ -0,0 +1,24 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: {
+        $exists: true,
+        $not: { $eq: "" },
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+    },
+  },
+  {
+    $project: {
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+      _id: 0,
+    },
+  },
+]);
