a62d94ffbdb801c9ece40ab9dfc400e3b1c5d90c
Hudson Carlos <hc88806@gmail.com>
2021-08-21 13:49:03 -0400

    challenge fiver done

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,5 +1,5 @@
 const arr = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
-use("aggregations");
+
 db.movies.aggregate([{ $match: {
   countries: { $all: ["USA"] },
   cast: { $exists: true },
