cd415bd50834a6574de3944959b135ff9bad6f13
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-08-21 09:02:12 -0300

    :sparkles: Requirement 01
    Desafio 1
    Ajude a Trybe a escolher um filme para a próxima noite! Baseado em uma pesquisa, decidimos que os filmes em potencial devem atender alguns critérios, vejamos:
    
    Retorne todos os filmes que satisfaça, através de uma pipeline, as condições abaixo
    
    	imdb.rating deve ser maior ou igual a 7;
    	genres não deve conter Crime ou Horror;
    	rated deve ser igual a PG ou G;
    	languages contém English e Spanish.
    	Utilize a coleção movies.
    
    Sua query deve retornar 41 documentos.

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,10 @@
-// Iniciando o projeto.
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+]);
