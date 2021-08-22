ab43527229345a3effc32a8427caabd83248cdc3
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-08-22 17:34:04 -0300

    req 11 done

---
+++ challenges/desafio11.js
@@ -0,0 +1,21 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      count: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$count",
+    }
+  },
+  { $limit: 1 },
+]);
