525a5b39e9c698788b6d7dee51cba34a08c2e45c
luanlsr <luanfswd@gmail.com>
2021-08-22 20:13:56 -0300

    feat: :sparkles: finished req 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,1 +1,29 @@
 // Desafio 14
+db.trips.aggregate([
+  {
+    $addFields: {
+      inMinutes: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 6e4] },
+    },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: { $avg: "$inMinutes" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMedia" },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+]);
