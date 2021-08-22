e72e5a25b30da562877ea9ae5e36d35d974869d3
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-08-22 18:19:03 -0300

    req 13 done

---
+++ challenges/desafio13.js
@@ -0,0 +1,37 @@
+const START_DAY = "2016-03-10T00:00:00.0Z";
+const END_DAY = "2016-03-11T00:00:00.0Z";
+
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate(START_DAY),
+        $lt: ISODate(END_DAY),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      tripAvgTime: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            60 * 1000,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      count: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$tripAvgTime" },
+    }
+  },
+]);
