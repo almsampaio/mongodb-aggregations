99327cf644aee8aa9404af40dcad481390536ec6
rafael <rafittu@outlook.com>
2021-08-22 11:05:26 -0300

    feat: req 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,16 @@
+db.trips.aggregate([
+  { $group: {
+    _id: { diaDaSemana: { $dayOfWeek: "$startTime" },
+      nomeEstacao: "$startStationName",
+    },
+    total: { $sum: 1 },
+  } },
+  { $match: { "_id.diaDaSemana": 5 } },
+  { $project: {
+    _id: 0,
+    nomeEstacao: "$_id.nomeEstacao",
+    total: "$total",
+  } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
