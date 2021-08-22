9ad3c00a11f33ba78b5bdf891d59699bb1a4713a
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-22 19:33:50 +0000

    feat: challenge 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,39 @@
+const favoriteActors = [
+  "Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney",
+];
+
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { countries: "USA" },
+        { "tomatoes.viewer.rating": { $gte: 3 } },
+        { cast: { $in: favoriteActors } },
+      ],
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: [favoriteActors, "$cast"],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  {
+    $project: {
+      title: 1,
+      _id: 0,
+    },
+  },
+  { $limit: 25 },
+  { $skip: 24 },
+]);
