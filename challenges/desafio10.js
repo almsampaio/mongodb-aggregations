31f6f4d03eb5374d502fbc64f48558647b48a3c1
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-22 18:40:14 -0300

    feat: challenge 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,34 @@
 /* Encontre a duração média de viagens por tipo de usuário.
 Exiba o valor em horas com apenas duas casas decimais
 Exiba a média de viagens ordenada de forma crescente. */
+db.trips.aggregate([
+  {
+    $project: {
+      _id: 0,
+      user_type: "$usertype",
+      travel_hours: {
+
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          { $multiply: [60, 60, 1000] },
+        ],
+
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$user_type",
+      duracao_Media: { $avg: "$travel_hours" },
+    },
+  },
+  {
+    $project: {
+      tipo: "$_id",
+      _id: 0,
+      duracaoMedia: {
+        $round: ["$duracao_Media", 2],
+      },
+    },
+  },
+]);
