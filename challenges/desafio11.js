6016a35ad66eb52b5c82b5e072ea59c4845341d3
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-22 20:12:26 -0300

    feat: challenge 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,1 +1,31 @@
 /* Determine qual o dia da semana com maior número de viagens iniciadas. */
+
+db.trips.aggregate([
+  {
+    $project: {
+      _id: 0,
+      dia_Da_Semana: {
+        $dayOfWeek: "$startTime",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$dia_Da_Semana",
+      tot: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$tot",
+    },
+  },
+  {
+    $sort: { total: -1 },
+  },
+  {
+    $limit: 1,
+  },
+]);
