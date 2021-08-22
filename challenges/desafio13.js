ce5dea3ec704f13576c2a619ba0b1437592c6d43
luanlsr <luanfswd@gmail.com>
2021-08-22 14:35:24 -0300

    feat: :sparkles: finished req 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,1 +1,25 @@
 // Desafio 13
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $gte: ISODate("2016-03-10"), $lt: ISODate("2016-03-11") },
+    },
+  },
+  {
+    $addFields: {
+      inMinutes: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 6e4] },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: { $avg: "$inMinutes" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
