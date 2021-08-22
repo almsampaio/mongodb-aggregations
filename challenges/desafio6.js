9c8108d545132e488b9e6687921f4628c5f616ae
guilhermemd <guilherme.md@gmail.com>
2021-08-22 18:15:17 -0300

    fix: :bug: req 06

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -9,17 +9,15 @@ db.movies.aggregate([
       menor_rating: { $min: "$imdb.rating" },
       media_rating: { $avg: "$imdb.rating" },
       desvio_padrao: { $stdDevSamp: "$imdb.rating" },
-    }
+    },
   },
   {
     $project: {
       _id: 0,
       maior_rating: 1,
       menor_rating: 1,
-      media_rating: { $round: ["$media_rating", 1]},
-      desvio_padrao: { $round: ["$desvio_padrao", 1]},
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
   },
 ]);
-
-
