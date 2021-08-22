77cb1cddca54bec43310e4b1e53cb9b7e5ee45b0
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-22 18:47:15 -0300

    desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,19 @@
+db.movies.aggregate([
+  { $match: {
+    $and: [
+      { "imdb.rating": { $gte: 7 } },
+      { genres: { $nin: ["Crime", "Horror"] } },
+      { $or: [{ rated: "PG" }, { rated: "G" }] },
+      { $and: [{ languages: "English" }, { languages: "Spanish" }] },
+    ],
+  } },
+  { $project: {
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+    _id: 0,
+  } },
+  { $sort: { ano: -1, notaIMDB: -1, titulo: 1 } },
+]);
