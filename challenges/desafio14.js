0bc9adb9aa74e6ba15fa072729abfcfdbc9e693a
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-22 14:13:45 -0300

    Req 14 - Mostra as 5 bikes mais utilizadas

---
+++ challenges/desafio14.js
@@ -0,0 +1,31 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      avgDurationMs: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: {
+        $ceil: {
+          $divide: ["$avgDurationMs", 1000 * 60],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+]);
