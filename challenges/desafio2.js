a7bef3902bc6afa937d756132b10fea350233929
Stanrley Barbosa <3011stan@gmail.com>
2021-08-22 20:03:15 -0300

    Fix lint - comments

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,5 +1,7 @@
-// 2 - Utilizando o mesmo pipeline anterior, retorne apenas os 
-// campos title, rated, imdb.rating, imdb.votes e year, modificando seus nomes para titulo, avaliado, notaIMDB, votosIMDB e ano
+// 2 - Utilizando o mesmo pipeline anterior, retorne apenas os
+// campos title, rated, imdb.rating, imdb.votes e year,
+// modificando seus nomes para titulo, avaliado, notaIMDB,
+// votosIMDB e ano
 // use("commerce");
 db.movies.aggregate( // 5
   [
