3204be3b4464e1bbcb0468d639fdb41b66ad185c
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-08-22 17:17:01 -0300

    req 10 done

---
+++ challenges/desafio10.js
@@ -0,0 +1,30 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      totalHours: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          1000 * 60 * 60,
+        ],
+      },
+    }
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      hoursAVG: { $avg: "$totalHours" }
+    }
+  },
+  {
+    $sort: {
+      hoursAVG: 1,
+    },
+  },
+  {
+    $project: {
+      tipo: "$_id",
+      _id: 0,
+      duracaoMedia: { $round: ["$hoursAVG", 2] },
+    },
+  },
+]);
