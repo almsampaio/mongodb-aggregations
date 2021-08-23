6e5be9c870996bb01bae78b0270825bb3f46bb0f
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-23 16:52:49 -0300

    [new] requisito 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,17 @@
+db.trips.aggregate([
+  { $project: {
+    dayOfWeek: { $dayOfWeek: "$startTime" },
+  } },
+  { $group: {
+    _id: "$dayOfWeek",
+    total: { $sum: 1 },
+  } },
+  { $project: {
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: "$total",
+  },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
