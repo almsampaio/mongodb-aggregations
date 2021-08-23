68fc313f38370a6706697358577176aa33dabf40
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-23 18:57:53 -0300

    Desafio 9 concluído

---
+++ challenges/desafio9.js
@@ -0,0 +1,10 @@
+db.trips.aggregate([{
+  $match:
+  { birthYear: { $ne: "", $exists: true } } },
+{ $group:
+{ _id: null,
+  maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+  menorAnoNascimento: { $min: { $toInt: "$birthYear" } } } },
+{ $project: { id: 0,
+  maiorAnoNascimento: 1,
+  menorAnoNascimento: 0 } }]);
