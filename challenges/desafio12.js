776e2bf52335913bd975e2bd890ab7ce2a7eeee2
Gabriel Ferreira <glucas709@gmail.com>
2021-08-23 19:27:00 -0300

    Challenge 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        diaDaSemana: { $dayOfWeek: "$startTime" },
+        nomeEstacao: "$startStationName",
+      },
+      total: {
+        $sum: 1,
+      },
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  { $limit: 1 },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.nomeEstacao",
+      total: "$total",
+    },
+  },
+]);
