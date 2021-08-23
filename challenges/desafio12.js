bc575cfad4e864336ddc7abb5cff16a5180a258c
Flavio Franco <flavio.win@gmail.com>
2021-08-23 13:13:39 -0300

    desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,8 @@
+db.trips.aggregate([
+  { $addFields: { weekDay: { $dayOfWeek: "$startTime" } } },
+  { $match: { weekDay: 5 } },
+  { $group: { _id: "$startStationName", count: { $sum: 1 } } },
+  { $sort: { count: -1 } },
+  { $limit: 1 },
+  { $project: { nomeEstacao: "$_id", total: "$count", _id: 0 } },
+]);
