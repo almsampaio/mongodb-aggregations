f22667b3e39f5d9fc06da100bd0677403773e610
Sergio Martins <sergiomos2010@gmail.com>
2021-08-23 18:37:58 -0300

    desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,6 @@
+db.movies.aggregate([
+  { $project: { title_split: { $split: ["$title", " "] } } },
+  { $match: { title_split: { $size: 1 } } },
+  { $sort: { title_split: 1 } },
+  { $project: { _id: 0 } },
+]);
