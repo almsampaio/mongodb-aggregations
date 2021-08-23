428fddad381f98eec360d66aa93ca4c3a7896b3e
mallmann02 <leomallmann020302@gmail.com>
2021-08-23 14:54:19 -0300

    style: lint auto fix

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,9 +1,9 @@
 const FAV_ACTORS = [
-  'Sandra Bullock',
-  'Tom Hanks',
-  'Julia Roberts',
-  'Kevin Spacey',
-  'George Clooney'
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
 ];
 
 db.movies.aggregate([
@@ -11,29 +11,29 @@ db.movies.aggregate([
     $match: {
       $and: [
         { countries: "USA" },
-        { "tomatoes.viewer.rating": {$gte: 3} },
-        { cast: { $in: FAV_ACTORS } }
-      ]
-    }
+        { "tomatoes.viewer.rating": { $gte: 3 } },
+        { cast: { $in: FAV_ACTORS } },
+      ],
+    },
   },
   {
     $addFields: {
-      num_favs: { $size: { $setIntersection: [FAV_ACTORS, "$cast"] } }
-    }
+      num_favs: { $size: { $setIntersection: [FAV_ACTORS, "$cast"] } },
+    },
   },
   {
     $sort: {
       num_favs: -1,
       "tomatoes.viewer.rating": -1,
-      title: -1
-    }
+      title: -1,
+    },
   },
   {
     $project: {
       _id: 0,
-      title: 1
-    }
+      title: 1,
+    },
   },
   { $limit: 25 },
-  { $skip: 24 }
+  { $skip: 24 },
 ]);
