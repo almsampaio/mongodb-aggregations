de604be3d2fd3dc14e29bfa3eb38aa479d603516
lucas-chave <lcchavesr150@gmail.com>
2021-08-23 16:41:48 -0300

    challenge 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,20 @@
+// challenge 5;
+
+const actors = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
+db.movies.aggregate(
+  [
+    { $match: { countries: { $in: ["USA"] }, "tomatoes.viewer.rating": { $gte: 3 }, cast: { $exists: true } } },
+    { $addFields: { num_favs: { $size: { $setIntersection: [actors, "$cast"] } } } },
+    { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+    { $project: { title: 1, _id: 0, num_favs: 1 } },
+    { $skip: 24 },
+    { $limit: 1 },
+  ],
+);
