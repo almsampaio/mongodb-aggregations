4a938a0481fe69b5d19a0878a8d29a5f348c99dc
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-23 16:55:02 -0300

    [fix] Eu tinha esquecido de adicionar o 3 e o 4 o.o

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+db.movies.aggregate([
+  { $addFields: {
+    title_split: { $split: ["$title", " "] },
+  } },
+  { $match: {
+    title_split: { $size: 1 },
+  } },
+  { $sort: {
+    title_split: 1,
+  } },
+  { $project: {
+    title_split: 1, _id: 0,
+  } },
+]);
