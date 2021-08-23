6b2b95613e8f93a480b1ffbe90bea31880c596bc
mallmann02 <leomallmann020302@gmail.com>
2021-08-23 14:51:02 -0300

    Challenges: 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,28 @@
+db.movies.aggregate([
+  {
+    $match: {
+      languages: 'English'
+    }
+  },
+  { $unwind: "$cast" },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" }
+    }
+  },
+  {
+    $sort: {
+      numeroFilmes: -1,
+      cast: -1
+    }
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] }
+    }
+  }
+]);
