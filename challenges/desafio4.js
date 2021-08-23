c1385e1f71c6dd917e7d5fce2b4f04200f71b874
Nathalia Zebral <natzebral@gmail.com>
2021-08-23 16:34:43 -0300

    corrigindo desafio 4 - split escrito errado

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,24 +1,6 @@
 db.movies.aggregate([
-  {
-    $addFields:
-    {
-      title_split: { $split: ["$title", " "] },
-    },
-  },
-  {
-    $match:
-    {
-      title_slpit: { $size: 1 },
-    },
-  },
-  {
-    $project:
-    {
-      _id: 0,
-      title_split: 1,
-    },
-  },
-  {
-    $sort: { title_split: 1 },
-  },
+  { $addFields: { title_split: { $split: ["$title", " "] } } },
+  { $match: { title_split: { $size: 1 } } },
+  { $project: { _id: 0, title_split: 1 } },
+  { $sort: { title_split: 1 } },
 ]);
