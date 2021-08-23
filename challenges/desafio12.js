566cd049b5120c5edacdb1c73b8406dedbbf4199
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-22 21:06:18 -0300

    feat: challenge 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,3 +1,36 @@
 /* Usando a pipeline anterior que retorna o dia com mais viagens,
 determine qual estação tem o maior número de viagens nesse dia da semana.
 Exiba apenas o nome da estação e o total de viagens. */
+db.trips.aggregate([
+  {
+    $project: {
+      _id: 0,
+      dia_Da_Semana: {
+        $dayOfWeek: "$startTime",
+      },
+      startStationName: 1,
+    },
+  },
+  {
+    $group: {
+      _id: {
+        dia_Da_Semana: "$dia_Da_Semana",
+        startStationName: "$startStationName",
+      },
+      tot: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { tot: -1 },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.startStationName",
+      total: "$tot",
+    },
+  },
+]);
