9169f1c216ca2d7cc9f4eae70d44fbaecdd5e27c
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-23 20:21:01 -0300

    Challenge 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,15 @@
+db.movies.aggregate([
+  { $match: { awards: /^Won\s\d+\sOscar/i } },
+  { $group: {
+    _id: "null",
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    media_rating: { $avg: "$imdb.rating" },
+    desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+  } },
+  { $project: { _id: false,
+    maior_rating: true,
+    menor_rating: true,
+    media_rating: { $round: ["$media_rating", 1] },
+    desvio_padrao: { $round: ["$desvio_padrao", 1] } } },
+]);
