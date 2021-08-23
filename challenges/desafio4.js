47edda5cf05caf93338311f02e8aca93b09a93a1
phlopes <phlopes93@gmail.com>
2021-08-23 15:32:07 -0300

    challenge 4 completed

---
+++ challenges/desafio4.js
@@ -0,0 +1,18 @@
+db.movies.aggregate([
+  {
+    $project: {
+      title_split: { $split: ["$title", " "] },
+      _id: 0,
+    },
+  },
+  {
+    $match: {
+      title_split: { $size: 1 },
+    },
+  },
+  {
+    $sort: {
+      title_split: 1,
+    },
+  },
+]);
