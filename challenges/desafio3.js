372b5a7bd7c0a3392e2355ce45b1351be57d5840
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-22 22:38:40 -0300

    Desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,26 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["G", "PG"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $sort: {
+      ano: -1,
+      notaIMDB: -1,
+      titulo: 1,
+    },
+  },
+]);
