e945fdaab08eba84c9b334f336ce0851ee84066c
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-23 08:24:14 -0300

    Feat: (req-6) Considering all the films that won the Oscar at least once, calculate the highest value, lowest value, mean and standard deviation of the ratings

---
+++ challenges/desafio6.js
@@ -0,0 +1,26 @@
+// use("aggregations");
+// db.movies.find();
+
+// use("aggregations");
+
+db.movies.aggregate([
+  { $match: { awards: { $regex: /Won \d+ Oscar/ } } },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      manor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      manor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
+]);
