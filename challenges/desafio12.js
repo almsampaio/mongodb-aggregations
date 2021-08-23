ec4b5d0b3c9d8435d9622ccf4ccf82e8aaaf29df
Mateus Alencar <mtauws@gmail.com>
2021-08-23 11:44:49 -0300

    chore: req 6, 10 and 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,8 @@
+/** Source: https://github.com/tryber/sd-09-mongodb-aggregations/tree/hannibal1207-mongodb-aggregations */
+db.trips.aggregate([
+  { $addFields: { dayOfWeek: { $dayOfWeek: "$startTime" } } },
+  { $group: { _id: { dayOfWeek: "$dayOfWeek", nomeEstacao: "$startStationName" }, count: { $sum: 1 } } },
+  { $sort: { count: -1 } },
+  { $limit: 1 },
+  { $project: { _id: 0, nomeEstacao: "$_id.nomeEstacao", total: "$count" } },
+]);
