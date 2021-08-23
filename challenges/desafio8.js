bcf0f476242591cabf9be22e102f763acb548107
Jodiel Briesemeister <jodielll29@gmail.com>
2021-08-23 16:22:14 -0300

    refactor: req08

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,26 +1,30 @@
-db.air_routes.aggregate([
-  {
-    $match: {
-      airplane: { $in: ["747", "380"] },
-    },
-  },
+db.air_alliances.aggregate([
   {
     $lookup: {
-      from: "air_alliances",
-      localField: "airline.name",
-      foreignField: "airlines",
-      as: "string",
+      from: "air_routes",
+      let: { airlinesArray: "$airlines" },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $and: [
+                { $in: ["$airline.name", "$$airlinesArray"] },
+                { $in: ["$airplane", ["747", "380"]] },
+              ],
+            },
+          },
+        },
+      ],
+      as: "routes",
     },
   },
-  { $unwind: "$string" },
+  { $unwind: "$routes" },
   {
     $group: {
-      _id: "$string.name",
+      _id: "$name",
       totalRotas: { $sum: 1 },
     },
   },
-  {
-    $sort: { totalRotas: -1 },
-  },
+  { $sort: { totalRotas: -1 } },
   { $limit: 1 },
 ]);
