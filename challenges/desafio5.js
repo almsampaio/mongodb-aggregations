821891d74a53102029b1682c61cf55a631edda73
lucas-chave <lcchavesr150@gmail.com>
2021-08-23 17:02:21 -0300

    fix challenge 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -13,7 +13,7 @@ db.movies.aggregate(
     { $match: { countries: { $in: ["USA"] }, "tomatoes.viewer.rating": { $gte: 3 }, cast: { $exists: true } } },
     { $addFields: { num_favs: { $size: { $setIntersection: [actors, "$cast"] } } } },
     { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
-    { $project: { title: 1, _id: 0, num_favs: 1 } },
+    { $project: { title: 1, _id: 0 } },
     { $skip: 24 },
     { $limit: 1 },
   ],
