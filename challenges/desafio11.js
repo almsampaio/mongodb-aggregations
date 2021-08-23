84a738b9afc110ceb89d3372f155b991d04677fe
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-23 20:18:05 -0300

    Feat: (req-11) Determine which day of the week has the highest number of trips initiated.

---
+++ challenges/desafio11.js
@@ -0,0 +1,21 @@
+// use("aggregations");
+// db.trips.find();
+
+// use("aggregations");
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+  { $limit: 1 },
+]);
