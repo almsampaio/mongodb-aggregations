f2278b9164159ae292f8ba9d7eb686afd1946af7
guilhermemd <guilherme.md@gmail.com>
2021-08-22 22:11:21 -0300

    feat: :sparkles: req 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,3 @@
-use("aggregations");
 db.trips.aggregate([
   {
     $addFields: {
@@ -18,8 +17,14 @@ db.trips.aggregate([
   },
   {
     $project: {
+      _id: 0,
       tipo: "$_id",
       duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: 1,
     },
   },
 ]);
