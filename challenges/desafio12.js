eee404325f15823b27095e2638e673d2328f5d97
gabrieldeori <deori.pessoal@gmail.com>
2021-08-23 17:02:55 -0300

    Add desafio12 OK

---
+++ challenges/desafio12.js
@@ -0,0 +1,37 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      DIASEMANA: {
+        $dayOfWeek: "$startTime",
+      },
+    },
+  },
+  {
+    $match: {
+      DIASEMANA: 5,
+    },
+  },
+  {
+    $group: {
+      _id: "$startStationName",
+      total: {
+        $sum: 1,
+      },
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "$total",
+    },
+  },
+]);
