6922b00d036ce028dca984434a1c877961c8789e
Gabriel Ferreira <glucas709@gmail.com>
2021-08-23 18:15:51 -0300

    Challenge 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,29 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $divide: [
+            {
+              $subtract: ["$stopTime", "$startTime"],
+            },
+            3600000,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: 1,
+    },
+  },
+]);
