f3b0be9bda5fbbf5a6855144601d6cc1a16eec42
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-23 20:34:55 -0300

    Challenge 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,11 @@
+db.movies.aggregate([
+  { $match: { languages: "English" } },
+  { $unwind: "$cast" },
+  { $group: {
+    _id: "$cast",
+    numeroFilmes: { $sum: 1 },
+    mediaIMDB: { $avg: "$imdb.rating" },
+  } },
+  { $project: { numeroFilmes: true, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
+]);
