87d759679a8ccdab91a6925525cb956560cdbcc1
mallmann02 <leomallmann020302@gmail.com>
2021-08-23 14:51:03 -0300

    Challenges: 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,28 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            1000 * 60,
+          ],
+        }
+      }
+    }
+  },
+  {
+    $sort: {
+      duracaoMedia: -1
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: {$ceil: "$duracaoMedia"}
+    }
+  },
+  { $limit: 5 }
+]);
