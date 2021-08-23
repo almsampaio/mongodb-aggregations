eeb426877cd1b0fda3ccc8512f585ea92f6a9875
Sergio Martins <sergiomos2010@gmail.com>
2021-08-23 18:09:26 -0300

    desafio 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,18 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $not: { $in: ["Crime", "Horror"] } },
+      rated: { $in: ["PG", "G"] },
+      languages: { $in: ["English", "Spanish"] },
+    },
+  },
+  { $project: {
+    _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+  } },
+]);
