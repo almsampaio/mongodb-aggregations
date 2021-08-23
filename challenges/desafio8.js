4104408aac358a3c477a1066a1952f5d33e12934
Gabriel Ferreira <glucas709@gmail.com>
2021-08-23 17:25:20 -0300

    Challenge 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,15 @@
+db.air_routes.aggregate([
+  { $match: { airplane: { $in: ["747", "380"] } } },
+  {
+    $lookup: {
+      from: "air_alliances",
+      localField: "airline.name",
+      foreignField: "airlines",
+      as: "airAlliances",
+    },
+  },
+  { $unwind: "$airAlliances" },
+  { $group: { _id: "$airAlliances.name", totalRotas: { $sum: 1 } } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+]);
