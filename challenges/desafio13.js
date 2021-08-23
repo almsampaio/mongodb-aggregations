7ee82a0b7f347fde1ebfa8c94339f9f7ad237e1f
Bruno Mendes <mendessbruno@gmail.com>
2021-08-23 14:32:25 -0300

    resolvido desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,17 @@
+db.trips.aggregate([
+  {
+    $match: {
+      $and: [
+        { startTime: { $gte: ISODate("2016-03-10T00:00:00Z") } },
+        { startTime: { $lt: ISODate("2016-03-11T00:00:00Z") } },
+      ],
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+    },
+  },
+  { $project: { _id: 0, duracaoMediaEmMinutos: { $ceil: { $divide: ["$duracaoMedia", (60000)] } } } },
+]);
