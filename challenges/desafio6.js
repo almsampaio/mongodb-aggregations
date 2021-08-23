bc8aacb16f003582f57c23088587250ff19851cc
Joao Nasc <oi@joaonasc.dev>
2021-08-23 19:59:19 -0300

    feat: challenge 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,26 @@
+const collection = db.movies;
+const conditions = {
+  awards: { $regex: /won \d+ oscar/i },
+};
+
+const group = {
+  _id: null,
+  maior_rating: { $max: "$imdb.rating" },
+  menor_rating: { $min: "$imdb.rating" },
+  media_rating: { $avg: "$imdb.rating" },
+  desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+};
+
+const result = {
+  _id: 0,
+  maior_rating: { $round: ["$maior_rating", 1] },
+  menor_rating: { $round: ["$menor_rating", 1] },
+  media_rating: { $round: ["$media_rating", 1] },
+  desvio_padrao: { $round: ["$desvio_padrao", 1] },
+};
+
+collection.aggregate([
+  { $match: conditions },
+  { $group: group },
+  { $project: result },
+]);
