a436d4af8a208e6adc0bc2c07c3f631e3e2bc17f
andy silva <andssilva.23@gmail.com>
2021-08-23 15:56:00 -0400

    feat(req 2): :rocket: challenge 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,25 @@
+// use("aggregations")
+
+// fazer $project dos seguintes pontos:
+// titulo: "$title"
+// avaliado: "$rated"
+// notaIMDB: "$imdb.rating"
+// votosIMDB: "$imdb.votes"
+// ano: "$year"
+
+db.movies.aggregate([
+  { $match: {
+    "imdb.rating": { $gte: 7 },
+    genres: { $nin: ["Crime", "Horror"] },
+    rated: { $in: ["PG", "G"] },
+    languages: { $all: ["English", "Spanish"] },
+  } },
+  { $project: {
+    _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+  } },
+]);
