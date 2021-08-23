a6e02094fcac54ae50c2336c966faaa4d5329768
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-08-23 15:48:41 -0300

    fix: def02 -removed id

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -4,6 +4,7 @@ db.movies.aggregate([{ $match:
       rated: { $in: ["PG", "G"] },
       languages: { $all: ["English", "Spanish"] },
     } }, { $project: {
+  _id: 0,
   titulo: "$title",
   avaliado: "$rated",
   notaIMDB: "$imdb.rating",
