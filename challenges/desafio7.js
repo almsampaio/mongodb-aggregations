d79eb31c4d982b9d3640d9caac6f031d1f035db9
carlos25xrm <carlos25xrm@gmail.com>
2021-08-23 18:08:54 -0300

    fix: desafio 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,11 +1,11 @@
 db.movies.aggregate([
   { $match: { languages: "English" } },
-  { $unwind: "$cast" }, 
+  { $unwind: "$cast" },
   { $group:
     {
       _id: "$cast",
       numeroFilmes: { $sum: 1 },
-      mediaIMDB: { $avg: "$imdb.rating" }
+      mediaIMDB: { $avg: "$imdb.rating" },
     },
   },
   { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
