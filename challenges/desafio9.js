e8a3c907f94d7dcd90d90a121e3abbcd64899e32
defauth98 <neto.daniribeiro@gmail.com>
2021-08-23 14:51:01 -0300

    req 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,5 @@
+db.trips.aggregate([
+  { $match: { birthYear: { $exists: true, $ne: "" } } },
+  { $group: { _id: 1, maiorAnoNascimento: { $max: { $toInt: "$birthYear" } }, menorAnoNascimento: { $min: { $toInt: "$birthYear" } } } },
+  { $project: { _id: 0 } },
+]);
