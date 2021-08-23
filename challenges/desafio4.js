aaec2d96a78c277c3d16f46ae3ccdd37224c1ec3
mallmann02 <leomallmann020302@gmail.com>
2021-08-23 19:00:31 -0300

    fix: pipeline phases order

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -10,14 +10,14 @@ db.movies.aggregate([
     },
   },
   {
-    $project: {
-      _id: 0,
-      title_split: 1,
+    $sort: {
+      title: 1,
     },
   },
   {
-    $sort: {
-      title: 1,
+    $project: {
+      _id: 0,
+      title_split: 1,
     },
   },
 ]);
