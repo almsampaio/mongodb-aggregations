2a3edb6c5ee7eb2d69179fd1ef1bfad9177f86bc
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-08-23 19:54:05 -0300

    feat: challenge 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,34 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: {
+          $subtract: [
+            "$stopTime",
+            "$startTime",
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: {
+        $ceil: [
+          { $divide: ["$duracaoMedia", 6e4] },
+        ],
+      },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+]);
