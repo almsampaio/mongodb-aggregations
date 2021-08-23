bcbdf53a268d580cab1071909e630aef92c7db62
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-23 16:41:27 -0300

    Challenge 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,21 @@
+db.movies.aggregate(
+  [{
+    $match:
+    { $and: [
+      { "imdb.rating": { $gte: 7 } },
+      { genres: { $nin: ["Crime", "Horror"] } },
+      { rated: { $in: ["PG", "G"] } },
+      { languages: "English" },
+      { languages: "Spanish" },
+    ] },
+  },
+  { $project: {
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+    _id: false,
+  } },
+  ],
+);
