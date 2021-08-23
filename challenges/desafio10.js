264c93a77074fd6fc8300bf6d2c36e6419d11c5a
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-23 13:00:08 -0300

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,21 @@
+db.trips.aggregate([
+  { $addFields:
+    {
+      dateDiff: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          3600000,
+        ],
+      },
+    },
+  },
+  { $group: {
+    _id: "$usertype",
+    duracaoMedia: { $avg: "$dateDiff" },
+  } },
+  { $project: {
+    _id: 0,
+    tipo: "$_id",
+    duracaoMedia: { $round: ["$duracaoMedia", 2] },
+  } },
+]);
