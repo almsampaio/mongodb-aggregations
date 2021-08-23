9a88e55b1a5c912c169d7489a27158e8f0a418cd
Eduardo Seije <eduardoseije@gmail.com>
2021-08-23 17:07:29 -0300

    Desafio 09 - convertendo birthYear com  e removendo espaço entre aspas duplas do

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -3,19 +3,21 @@ db.trips.aggregate(
     { $match: {
       $and: [
         { birthYear: { $exists: true } },
-        { birthYear: { $ne: " " } },
+        { birthYear: { $ne: "" } },
       ],
+    } },
+    { $addFields: {
+      intBirthYear: { $toInt: "$birthYear" },
     } },
     { $group: {
       _id: null,
-      maiorAnoNascimento: { $max: "$birthYear" },
-      menorAnoNascimento: { $min: "$birthYear" },
+      maiorAnoNascimento: { $max: "$intBirthYear" },
+      menorAnoNascimento: { $min: "$intBirthYear" },
     } },
     { $project: {
       _id: 0,
       maiorAnoNascimento: 1,
       menorAnoNascimento: 1,
     } },
-
   ],
 );
