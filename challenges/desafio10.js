cec71e5fa2046bc267ca4d99ca9214bb40b80aa6
Bruno Mendes <mendessbruno@gmail.com>
2021-08-23 14:50:26 -0300

    resolvido problema de ordenaçao o ex 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -5,5 +5,6 @@ db.trips.aggregate([
       duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
     },
   },
+  { $sort: { duracaoMedia: 1 } },
   { $project: { tipo: "$_id", _id: 0, duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", (3600000)] }, 2] } } },
 ]);
