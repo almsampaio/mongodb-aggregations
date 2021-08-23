a36d175dd1eb08e6a5b5300a5970bae76a7c9906
rogerio-lambert <rogerio.cl@gmail.com>
2021-08-23 14:41:30 -0300

    desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,10 @@
+db.movies.aggregate([
+  { $unwind: "$cast" },
+  { $group: {
+    _id: "$cast",
+    numeroFilmes: { $sum: 1 },
+    mediaIMDB: { $avg: "$imdb.rating" },
+  } },
+  { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
+]);
