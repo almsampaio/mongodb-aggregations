e32285c89178a80b0894b33ee594253c985041dc
Marcus C. H. Filho <mchusemann@gmail.com>
2021-08-23 15:05:29 -0300

    feat: desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,21 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00.000Z"),
+        $lt: ISODate("2016-03-11T00:00:00.0Z"),
+      },
+    },
+  },
+  {
+    $addFields: {
+      duracaoEmMinutos: {
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
+      },
+    },
+  },
+  {
+    $group: { _id: null, duracaoMediaEmMinutos: { $avg: "$duracaoEmMinutos" } },
+  },
+  { $project: { _id: 0, duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" } } },
+]);
