3602cf51f178ccdd144be6047030e7df3394321c
guilhermemd <guilherme.md@gmail.com>
2021-08-23 10:46:14 -0300

    feat: :sparkles: req 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  { 
+    $addFields: {
+      dayOfWeek: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $group: { 
+      _id: "$dayOfWeek",
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $project:{
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$count",
+    },
+  },
+  {
+    $sort: { total: -1 },
+  },
+  {
+    $limit: 1,
+  },
+]);
