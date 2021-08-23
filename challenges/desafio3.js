7f33f885beb5734931023530ffe131e66e3173a7
Patrick Morais <ppternunes@gmail.com>
2021-08-23 20:36:35 -0300

    req 1 ao 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,26 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $sort: {$and: [
+      { notaIMDB: -1, ano: -1 },
+      {titulo: 1}
+    ]},
+  },
+]);
