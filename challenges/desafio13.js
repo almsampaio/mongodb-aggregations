5524f445f627c11b8180c0278d422eeba3738a7d
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-22 21:33:55 -0300

    feat: challenge 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,2 +1,33 @@
 /* Determine a duração média das viagens iniciadas no dia 10/03/2016, em minutos.
 Arredonde o resultado para cima. */
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $gte: ISODate("2016-03-10"), $lt: ISODate("2016-03-11") },
+    },
+  },
+  {
+    $addFields: {
+      duracaoViage_em_Min: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          60000,
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMedia: {
+        $avg: "$duracaoViage_em_Min",
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMedia" },
+    },
+  },
+]);
