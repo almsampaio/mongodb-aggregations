7099f3810c5e88d8aa1d38ca2b0c849f7a0116ad
mallmann02 <leomallmann020302@gmail.com>
2021-08-23 14:54:19 -0300

    style: lint auto fix

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,28 +1,28 @@
 db.movies.aggregate([
   {
     $match: {
-      languages: 'English'
-    }
+      languages: "English",
+    },
   },
   { $unwind: "$cast" },
   {
     $group: {
       _id: "$cast",
       numeroFilmes: { $sum: 1 },
-      mediaIMDB: { $avg: "$imdb.rating" }
-    }
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
   },
   {
     $sort: {
       numeroFilmes: -1,
-      cast: -1
-    }
+      cast: -1,
+    },
   },
   {
     $project: {
       _id: 1,
       numeroFilmes: 1,
-      mediaIMDB: { $round: ["$mediaIMDB", 1] }
-    }
-  }
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
 ]);
