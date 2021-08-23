82a37e968bff600d3558d5f1c67e015e51286a10
Alexandre Oliveira <alexandremolive@gmail.com>
2021-08-23 14:57:19 -0300

    feat req5

---
+++ challenges/desafio5.js
@@ -0,0 +1,40 @@
+const favorites = [
+  "Sandra Bullock", "Tom Hanks", "Julia Roberts",
+  "Kevin Spacey", "George Clooney"];
+
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { cast: { $in: favorites } },
+        { countries: "USA" },
+        { "tomatoes.viewer.rating": { $gte: 3 } },
+      ],
+    } },
+  { $addFields: {
+    num_favs: {
+      $size: {
+        $setIntersection: [favorites, "$cast"],
+      },
+    } },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  { $skip: 24 },
+  { $limit: 1 },
+
+  { $project: {
+    title: 1,
+    _id: 0,
+  } },
+
+]);
+
+// para a concluão deste requisito realizei consulta no
+// repositório do colega
+// viniciusfranca019
