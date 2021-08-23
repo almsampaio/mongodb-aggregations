78825bb7192b485f3a0b742016154e7d43f6a772
igson <igson660@gmail.com>
2021-08-23 16:10:43 -0500

    desafio 02

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,1 +1,22 @@
-
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } },
+      ],
+    },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+      _id: 0,
+    },
+  },
+]);
