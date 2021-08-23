44d6243eb4db448b71127db4c9ae491577be0639
vinicios <soicinivt@gmail.com>
2021-08-23 17:34:08 -0300

    Desafio 2 - rename some keys.

---
+++ challenges/desafio2.js
@@ -0,0 +1,49 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+    },
+  },
+  {
+    $match: {
+      genres: { $nin: [
+        "Crime",
+        "Horror",
+      ] },
+    },
+  },
+  {
+    $match: {
+      rated: {
+        $in: [
+          "PG",
+          "G",
+        ],
+      },
+    },
+  },
+  {
+    $match: {
+      $and: [
+        { languages: "English" },
+        { languages: "Spanish" },
+      ],
+    },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+      _id: 0,
+    },
+  },
+  // {
+  //   $limit: 1,
+  // },
+  // {
+  //   $count: "numberOfDocuments",
+  // },
+]);
