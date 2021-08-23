9a76775a9efdd6dfbb8ad2069cb3f50516789cd2
MaxRudim <max.hermsdorff@gmail.com>
2021-08-23 15:51:55 -0300

    Reqs. 09 ~ 10

---
+++ challenges/desafio9.js
@@ -0,0 +1,16 @@
+db.trips.aggregate([
+  {
+    $match: { birthYear: { $exists: true, $ne: "" } },
+  },
+  {
+    $group: {
+      _id: 1,
+      maiorAnoNascimento: {
+        $max: { $toInt: "$birthYear" },
+      },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" },
+      },
+    },
+  },
+  { $project: { _id: 0 } },
+]);
