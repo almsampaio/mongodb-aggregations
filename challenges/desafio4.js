954e240c9a9e478362f4d033b29b0115d5b5bfaf
Joao Nasc <oi@joaonasc.dev>
2021-08-23 18:33:25 -0300

    feat: challenge 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,13 @@
+const collection = db.movies;
+
+collection.aggregate([
+  {
+    $project: { _id: 0, title_split: { $split: ["$title", " "] } },
+  },
+  {
+    $match: { title_split: { $size: 1 } },
+  },
+  {
+    $sort: { title_split: 1 },
+  },
+]);
