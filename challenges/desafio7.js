7b9a991d5cc3db6bddb757d05958db7b58885e37
Patrick Morais <ppternunes@gmail.com>
2021-08-23 21:42:37 -0300

    req 7 ao 9

---
+++ challenges/desafio7.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  {
+    $match: {
+      languages: { $in: ["English"] },
+    },
+  },
+  { $unwind: "$cast" },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
+]);
