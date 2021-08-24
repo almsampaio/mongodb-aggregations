20211db126deda20584dd3c9249ec40bc5928436
Edrei <edreimateus@gmail.com>
2021-08-23 21:48:15 -0300

    feat: 7 requiriment

---
+++ challenges/desafio7.js
@@ -0,0 +1,17 @@
+db.movies.aggregate([
+  {
+    $unwind: "$cast",
+  },
+  {
+    $match: { languages: "English" },
+  },
+  {
+    $group: { _id: "$cast", numeroFilmes: { $sum: 1 }, mediaIMDB: { $avg: "$imdb.rating" } },
+  },
+  {
+    $addFields: { mediaIMDB: { $round: ["$mediaIMDB", 1] } },
+  },
+  {
+    $sort: { numeroFilmes: -1, _id: -1 },
+  },
+]);
