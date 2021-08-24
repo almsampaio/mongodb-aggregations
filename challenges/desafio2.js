ed5f328c75435c3abd2840a9c989ce2b6e32f5a5
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-08-23 21:09:14 -0300

    Fix 2 + desafio 12

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,12 +1,10 @@
 db.movies.aggregate([
   {
     $match: {
-      $and: [
-        { "imdb.rating": { $gte: 7 } },
-        { genres: { $nin: ["Crime", "Horror"] } },
-        { rated: { $in: ["PG", "G"] } },
-        { languages: { $all: ["English", "Spanish"] } },
-      ],
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
     },
   },
   {
@@ -14,8 +12,8 @@ db.movies.aggregate([
       _id: 0,
       titulo: "$title",
       avaliado: "$rated",
-      notaIMBD: "$imbd.rating",
-      votosIMBD: "$imbd.votes",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
       ano: "$year",
     },
   },
