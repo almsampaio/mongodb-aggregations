9d480ba6abaa9c3c161fea7c80ed93ada46a46e5
Edrei <edreimateus@gmail.com>
2021-08-23 22:00:37 -0300

    feat: 9 requiriment

---
+++ challenges/desafio9.js
@@ -0,0 +1,22 @@
+db.trips.aggregate([
+  {
+    $match: {
+      $and: [
+        { birthYear: { $exists: 1 } },
+        { birthYear: { $ne: "" } },
+      ],
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+    },
+  },
+]);
