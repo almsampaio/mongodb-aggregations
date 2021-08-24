10404c40bac80f3015b2440853a9bdeda3c11fc1
marcela <marcela9908@gmail.com>
2021-08-23 21:58:50 -0300

    Desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,9 @@
+db.air_routes.aggregate([
+  { $match: { airplane: { $in: ["747", "380"] } } },
+  { $lookup: { from: "air_alliances", localField: "airline.name", foreignField: "airlines", as: "alliance" } },
+  { $match: { "alliance.name": { $exists: true } } },
+  { $unwind: "$alliance" },
+  { $group: { _id: "$alliance.name", totalRotas: { $sum: 1 } } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+]);
