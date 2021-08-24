6ad7774ca1d0336f27ef0a784ed4aea57908f418
Sergio Martins <sergiomos2010@gmail.com>
2021-08-23 21:01:19 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,29 @@
+const favoritesActors = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
+db.movies.aggregate([
+  { $match: {
+    cast: { $in: favoritesActors },
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+  } },
+  { $addFields: {
+    num_fav: { $size: { $setIntersection: ["$cast", favoritesActors] } },
+  } },
+  { $sort: {
+    num_fav: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
+  } },
+  { $project: {
+    _id: 0,
+    title: 1,
+  } },
+  { $skip: 24 },
+  { $limit: 1 },
+]);
