b9ae8290e0e4dd67195a35e7c51f4f019866e8dc
rcalvs <rafaelcalvette@gmail.com>
2021-08-24 13:42:24 -0300

    req 1-4

---
+++ challenges/desafio1.js
@@ -0,0 +1,8 @@
+db.movies.aggregate([
+  { $match: {
+    "imdb.rating": { $gte: 7 },
+    genres: { $nin: ["Crime", "Horror"] },
+    rated: { $in: ["PG", "G"] },
+    languages: { $all: ["English", "Spanish"] },
+  } },
+]);
