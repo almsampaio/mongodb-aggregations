54b1f3f2a26c07db30302d305df8f4e484e53601
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-08-24 01:28:50 -0300

    Requisito 13 - completo

---
+++ challenges/desafio13.js
@@ -0,0 +1,29 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00Z"),
+        $lte: ISODate("2016-03-10T23:59:59Z"),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            60000,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
