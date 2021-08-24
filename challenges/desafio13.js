b22730dd27261cc3911a282382f84bfdffae37cd
Edrei <edreimateus@gmail.com>
2021-08-23 22:29:22 -0300

    feat: 13 requiriment

---
+++ challenges/desafio13.js
@@ -0,0 +1,17 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $gte: ISODate("2016-03-10T00:00:00.0Z"), $lt: ISODate("2016-03-11T00:00:00.0Z") },
+    },
+  },
+  {
+    $addFields: {
+      duration: { $round: [{ $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000] }, 2] },
+      date: { $dateToString: { format: "%Y-%m-%d", date: "$startTime" } } } },
+  {
+    $group: { _id: "$date", duracaoMediaEmMinutos: { $avg: "$duration" } },
+  },
+  {
+    $project: { _id: 0, duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" } },
+  },
+]);
