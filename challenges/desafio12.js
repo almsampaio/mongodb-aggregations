50b7683ff001a770255a874cb630f6cefe92912c
Maria Baeta <m.martinsdg@gmail.com>
2021-08-24 01:00:27 -0300

    feat: :rocket: Requisito 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,20 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        diaDaSemana: { $dayOfWeek: "$startTime" },
+        estacao: "$startStationName",
+      },
+      qtn: { $count: {} },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.estacao",
+      total: "$qtn",
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
