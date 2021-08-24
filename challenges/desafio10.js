5359b6122beffd026d0998f71f212de7241b4277
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-08-24 19:52:52 -0300

    desafio 6 e 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,18 +1,27 @@
 db.trips.aggregate([
-
+  {
+    $addFields: {
+      duracao: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] },
+    },
+  },
   {
     $group: {
       _id: "$usertype",
-      duracaoMedia: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] } },
+      duracaoMedia: { $avg: "$duracao" },
     },
   },
   {
     $project: {
-      tipo: $usertype,
+      tipo: "$_id",
       duracaoMedia: { $round: ["$duracaoMedia", 2] },
       _id: 0,
     },
   },
+  {
+    $sort: {
+      duracaoMedia: 1,
+    },
+  },
 ]);
 
-// https://stackoverflow.com/questions/41138877/how-to-calculate-timestamp-difference-in-mongodb-in-hours 
+// https://stackoverflow.com/questions/41138877/how-to-calculate-timestamp-difference-in-mongodb-in-hours
