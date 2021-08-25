0f6b43d948beb7c6bdc48bdd30ee64f90d47be73
Luiz <luizfvaccarif@gmail.com>
2021-08-25 10:53:17 -0300

    Feat: challenge 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,27 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      $or: [{ rated: { $eq: "PG" } }, { rated: { $eq: "G" } }],
+      $and: [{ languages: { $in: ["English"] } }, { languages: { $in: ["Spanish"] } }],
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $sort: {
+      ano: -1,
+      notaIMDB: -1,
+      titulo: 1,
+    },
+  },
+]);
