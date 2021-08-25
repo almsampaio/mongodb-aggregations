3c5d337980401ff452b584f2aec2d24750e52fae
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-25 17:07:08 -0300

    Fix: Desafio1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,10 +1,8 @@
-db.movies.aggregate(
-  [
-    { $match: {
-      $and: [
-        { "imdb.rating": { $gte: 7 } },
-        { genres: { $nin: ["Crime", "Horror "] } },
-        { rated: { $in: ["PG", "G"] } },
-        { languages: { $in: ["English", "Spanish"] } },
-      ] } }],
-);
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    } }]);
