04354400c10550c4ee444815b437cca07b365beb
Joao Nasc <oi@joaonasc.dev>
2021-08-25 16:37:00 -0300

    feat: challenge 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,15 @@
+const collection = db.trips;
+
+collection.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60 * 1000] },
+      },
+    },
+  },
+  { $project: { _id: 0, bikeId: "$_id", duracaoMedia: { $ceil: "$duracaoMedia" } } },
+  { $sort: { duracaoMedia: -1 } },
+  { $limit: 5 },
+]);
