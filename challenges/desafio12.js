28604eaa1a645b17c01ef8ce54fd6658b42c0044
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-25 20:28:51 -0300

    Desafio 12 OK

---
+++ challenges/desafio12.js
@@ -0,0 +1,33 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      weekDay: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $match: {
+      weekDay: 5,
+    },
+  },
+  {
+    $group:{
+      _id: "$startStationName",
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "$total",
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
