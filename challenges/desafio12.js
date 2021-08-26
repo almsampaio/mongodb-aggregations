010997cb7bb185dfcb2f67a13a2e0440882029b7
Debora Passos <deborinhapassos@gmail.com>
2021-08-26 19:50:18 -0300

    Desafios 12 e 13

---
+++ challenges/desafio12.js
@@ -0,0 +1,18 @@
+db.trips.aggregate([
+  { $addFields: { dayOfWeek: { $dayOfWeek: "$startTime" } } },
+
+  { $match: { dayOfWeek: 5 } },
+
+  { $group: {
+    _id: "$startStationName",
+    total: { $sum: 1 },
+  } },
+
+  { $project: {
+    _id: 0,
+    nomeEstacao: "$_id",
+    total: "$total" } },
+
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
