d6bdc994257f2f97d20f79635a23d6281efffdc9
igson <igson660@gmail.com>
2021-08-26 18:30:26 -0500

    desafio 09

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,1 +1,14 @@
+db.trips.aggregate([
+  { $match: { birthYear: { $exists: 1, $ne: "" } } },
 
+  { $addFields: { year: { $toInt: "$birthYear" } } },
+
+  {
+    $group: {
+      _id: 1,
+      maiorAnoNascimento: { $max: "$year" },
+      menorAnoNascimento: { $min: "$year" } },
+  },
+
+  { $project: { _id: 0 } },
+]);
