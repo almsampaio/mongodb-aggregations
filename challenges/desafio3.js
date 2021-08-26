093e6a88892aec6b51ae036eb91d1bcddd019710
Willian Alves <willian.alvesb65@gmail.com>
2021-08-26 15:54:58 -0300

    feat: Desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,33 @@
+// Desafio 3
+/* use("aggregations"); */
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        {
+          "imdb.rating": { $gte: 7 }, /* maior ou igual */
+        },
+        {
+          genres: { $nin: ["Crime", "Horror"] }, /* não deve conter  */
+        },
+        {
+          rated: { $in: ["PG", "G"] }, /* deve ser igual */
+        },
+        {
+          languages: { $all: ["English", "Spanish"] }, /* Todos que contém */
+        },
+      ],
+    },
+  },
+  {
+    $project: { /* Efetuando projeção */
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  { $sort: { ano: -1, notaIMDB: -1 } },
+]);
