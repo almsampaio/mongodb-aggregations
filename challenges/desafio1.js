2bdc5067008119b916cb9df3d484ceaf0203ed2f
Willian Alves <willian.alvesb65@gmail.com>
2021-08-26 09:45:02 -0300

    feat: Desafio 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,22 @@
+// Desafio 1
+use("aggregations");
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        {
+          "imdb.rating": { $gte: 7 }, /* maior ou igual */
+        },
+        {
+          genres: { $nin: ["Crime", "Horror"] }, /* não deve conter  */
+        },
+        {
+          rated: { $in: ["PG", "G"] }, /* deve ser igual */
+        },
+        {
+          languages: { $all: ["English", "Spanish"] }, /* Todos que contém */
+        },
+      ],
+    },
+  },
+]);
