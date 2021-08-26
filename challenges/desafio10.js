85419d6b576c983ba59d07ce0de535be72a661c9
igson <igson660@gmail.com>
2021-08-26 18:47:05 -0500

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,21 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"] } } },
+  },
+
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [{ $divide: ["$duracaoMedia", 60 ** 2 * 1000] }, 2],
+      },
+    },
+  },
+
+  { $sort: { duracaoMedia: 1 } },
+]);
