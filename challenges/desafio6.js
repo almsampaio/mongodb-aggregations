fc6f03e7e3cabf445acd65dad7131ef32ffe82e1
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-08-26 12:27:09 -0300

    feat:def06

---
+++ challenges/desafio6.js
@@ -0,0 +1,15 @@
+db.movies.aggregate([{ $match: { awards: { $regex: /won (\d?[1-9]|[1-9]0) oscar/gi } } },
+  { $group: {
+    _id: null,
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    media_rating: { $avg: "$imdb.rating" },
+    desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+  } },
+  { $project: {
+    _id: 0,
+    maior_rating: 1,
+    menor_rating: 1,
+    media_rating: { $round: ["$media_rating", 1] },
+    desvio_padrao: { $round: ["$desvio_padrao", 1] },
+  } }]);
