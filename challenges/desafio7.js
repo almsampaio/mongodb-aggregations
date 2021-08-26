973373d41f5667abc66e4ec36a8ea197fa930fc9
jonathan <jonathanfernandesjf98@gmail.com>
2021-08-26 15:13:49 -0300

    comentarios desafio 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -14,3 +14,10 @@ db.movies.aggregate([
   } },
   { $sort: { numeroFilmes: -1, _id: -1 } },
 ]);
+
+// primeiro utilizei o unwind para separar os atores dos filmes em que cast era um array,
+// fazendo assim um documento para cada ator.
+// depois com o match, eu filtrei apenas os atores cujo filme estivesse com o idioma em inglês.
+// por ultimo, com o group, eu juntei todos os valores passando como parâmetro para o group,
+// o nome do ator/atriz. Assim, podendo calcular em quantos filmes cada um aparecia e também
+// sua nota média no imdb.
