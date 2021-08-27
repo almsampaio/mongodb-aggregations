11e1fd124ede8a45e86a10e388687306090f28cd
Luiz <luizfvaccarif@gmail.com>
2021-08-27 14:52:09 -0300

    Feat: challenge 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  {
+    $match: { awards: { $regex: /won\s\d\soscar/i } },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: { $round: ["$imdb.rating", 1] } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
+]);
