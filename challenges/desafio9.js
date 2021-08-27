37cf12ede849e1f22f1caeaec19b9498296c3fa3
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-08-27 20:41:52 -0300

    :sparkles: Requirement 09
    Desafio 9
    A partir da coleção trips, determine o menor e o maior ano de nascimento.
    Guarde essa informação, você precisará dela mais tarde.
    
    Não considere documentos com valores vazios ("") e em que o campo não existe!
    
    Para este desafio utilize o operador $toInt para converter de string para valor inteiro.
    
    O resultado da sua query deve ter exatamente o seguinte formato (incluindo a ordem dos campos):
    
    { "maiorAnoNascimento" : <ano>, "menorAnoNascimento" : <ano> }

---
+++ challenges/desafio9.js
@@ -0,0 +1,29 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: {
+        $ne: "",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: {
+        $max: {
+          $toInt: "$birthYear",
+        },
+      },
+      menorAnoNascimento: {
+        $min: {
+          $toInt: "$birthYear",
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+    },
+  },
+]);
