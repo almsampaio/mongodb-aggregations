1ea4f3f83416e7e3062ef2c4ff67c14702547db4
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-08-27 16:17:15 -0300

    10th requirement completed

---
+++ challenges/desafio10.js
@@ -0,0 +1,18 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      time: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: [{ $divide: ["$time", 360000] }, 2] },
+    },
+  },
+  {
+    $sort: { duracaoMedia: 1 },
+  },
+]);
