229dec5b1c423071b00d6389583866790bfcc6ed
carlos25xrm <carlos25xrm@gmail.com>
2021-08-26 21:55:09 -0300

    fix: desafio 9 - Corrigindo erros do Eslint

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,13 +1,13 @@
 db.trips.aggregate([
   { $match:
-    { birthYear: { $exists: 1 , $ne: "" } },
+    { birthYear: { $exists: 1, $ne: "" } },
   },
   { $group:
     {
       _id: null,
-      "maiorAnoNascimento": { $max: { $toInt: "$birthYear" } },
-      "menorAnoNascimento": { $min: { $toInt: "$birthYear" } },
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
     },
   },
-  { $project: { "maiorAnoNascimento": 1, "menorAnoNascimento": 1, _id: 0 } },
+  { $project: { maiorAnoNascimento: 1, menorAnoNascimento: 1, _id: 0 } },
 ]);
