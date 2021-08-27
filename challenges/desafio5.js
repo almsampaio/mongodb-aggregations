674d0cff176d7c1590526f8e719c964e033c5a90
Luiz <luizfvaccarif@gmail.com>
2021-08-27 12:07:23 -0300

    Feat: challenge 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,35 @@
+const actorsList = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $ne: null },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: { $size: { $setIntersection: ["$cast", actorsList] } },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
+]);
