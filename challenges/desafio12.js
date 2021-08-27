4a3d7feebb347ec94de88fdae9a1c1ec4afe44b0
igson <igson660@gmail.com>
2021-08-26 19:12:45 -0500

    desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,19 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        weekDay: { $dayOfWeek: "$startTime" },
+        StationName: "$startStationName",
+      },
+      total: { $sum: 1 } } },
 
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.StationName",
+      total: "$total" } },
+
+  { $sort: { total: -1 } },
+
+  { $limit: 1 },
+]);
