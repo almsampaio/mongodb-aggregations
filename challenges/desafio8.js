71e79c99bed58739b91e6cc07d2d7b8e3ee510b9
biazidioti <bzferreira.bio@gmail.com>
2021-08-28 12:40:30 -0300

    Challenge 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,16 @@
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "airRoutes",
+    },
+  },
+  { $unwind: "$airRoutes" },
+  { $match: { "airRoutes.airplane": { $in: ["747", "380"] } } },
+  { $group: { _id: "$name", totalRotas: { $count: {} } } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+]);
