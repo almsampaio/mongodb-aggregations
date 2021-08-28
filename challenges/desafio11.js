d8c0bb32d09b1e250927008ca9ca3c9eb9ac1132
biazidioti <bzferreira.bio@gmail.com>
2021-08-28 12:47:25 -0300

    ESLINT

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -2,7 +2,7 @@ db.trips.aggregate([
   { $group: {
     _id: { $dayOfWeek: "$startTime" },
     totalTrips: { $sum: 1 },
-  }, },
+  } },
   { $project: { _id: 0, diaDaSemana: "$_id", total: "$totalTrips" } },
   { $sort: { total: -1 } },
-  { $limit: 1 }, ]);
+  { $limit: 1 }]);
--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,17 +1,17 @@
 db.trips.aggregate([
   { $group: {
-      _id: {
-        diaDaSemana: { $dayOfWeek: "$startTime" },
-        estacao: "$startStationName",
-      },
-      qtn: { $count: {} },
+    _id: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+      estacao: "$startStationName",
     },
+    qtn: { $count: {} },
+  },
   },
   { $project: {
-      _id: 0,
-      nomeEstacao: "$_id.estacao",
-      total: "$qtn",
-    },
+    _id: 0,
+    nomeEstacao: "$_id.estacao",
+    total: "$qtn",
+  },
   },
   { $sort: { total: -1 } },
   { $limit: 1 },
