906560708a1ae6ca1d0f27cb8f51cdacb7a058e5
biazidioti <bzferreira.bio@gmail.com>
2021-08-28 12:41:27 -0300

    Challenge 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,6 @@
+db.trips.aggregate([
+  { $addFields: { duracaoMedia: { $subtract: ["$stopTime", "$startTime"] } } },
+  { $group: { _id: "$usertype", duracaoMedia: { $avg: { $divide: ["$duracaoMedia", 3600000] } } } },
+  { $project: { _id: 0, tipo: "$_id", duracaoMedia: { $round: ["$duracaoMedia", 2] } } },
+  { $sort: { duracaoMedia: 1 } },
+]);
