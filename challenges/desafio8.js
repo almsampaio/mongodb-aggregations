040d0ae330debc6b8e7f0190ed5f10bda649b674
Willian Alves <willian.alvesb65@gmail.com>
2021-08-28 11:50:01 -0300

    feat: desadio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,20 @@
+// Desafio 7
+/* use("aggregations"); */
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  { $lookup: {
+    from: "air_routes",
+    localField: "airlines",
+    foreignField: "airline.name",
+    as: "routes",
+  } },
+  { $unwind: "$routes" },
+  {
+    $match: { "routes.airplane": { $in: ["747", "380"] } },
+  },
+  {
+    $group: { _id: "$name", totalRotas: { $sum: 1 } },
+  },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+]);
