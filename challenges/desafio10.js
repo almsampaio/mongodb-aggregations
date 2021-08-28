975bf22f48a09a58b41026b82688ff8311079762
phlopes <phlopes93@gmail.com>
2021-08-27 21:38:55 -0300

    challenge 10 completed

---
+++ challenges/desafio10.js
@@ -0,0 +1,21 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2],
+      },
+    },
+  },
+]);
