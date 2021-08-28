ba70feda874565ccd671e2932167aae089241ba2
Rafael <rafaelsantanafernandes@gmail.com>
2021-08-28 07:28:18 -0300

    done five

---
+++ challenges/desafio3.js
@@ -0,0 +1,18 @@
+db.movies.aggregate([
+  { $match: {
+    "imdb.rating": { $gte: 7 },
+    genres: { $nin: ["Crime", "Horror"] },
+    rated: { $in: ["PG", "G"] },
+    languages: { $all: ["English", "Spanish"] },
+  } },
+
+  { $project: {
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+    _id: false,
+  } },
+  { $sort: { ano: -1, notaIMDB: -1, titulo: 1 } },
+]);
---
+++ challenges/desafio4.js
@@ -0,0 +1,5 @@
+db.movies.aggregate([
+  { $project: { title_split: { $split: ["$title", " "] }, _id: false } },
+  { $match: { title_split: { $size: 1 } } },
+  { $sort: { title_split: 1 } },
+]);
