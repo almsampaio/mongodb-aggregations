126402830144490bc09c2fb2e9ecd94160b7d953
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-08-28 15:50:09 -0300

    feat: req08 e 09

---
+++ challenges/desafio9.js
@@ -0,0 +1,20 @@
+db.trips.aggregate([{ $match: { birthYear: { $exists: true, $not: { $eq: "" } } } },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: {
+        $max: { $toInt: "$birthYear" },
+      },
+      menorAnoNascimento: {
+        $min: { $toInt: "$birthYear" },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: false,
+      maiorAnoNascimento: true,
+      menorAnoNascimento: true,
+    },
+  },
+]);
