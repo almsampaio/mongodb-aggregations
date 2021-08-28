48131a25ef0fad70479b2d84aefc22ed3613250a
phlopes <phlopes93@gmail.com>
2021-08-27 21:57:20 -0300

    challenge 11 completed

---
+++ challenges/desafio11.js
@@ -0,0 +1,18 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
