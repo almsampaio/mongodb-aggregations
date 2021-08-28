cd926c76a991aab694eb7928294b69a39fdb0280
Rafael <rafaelsantanafernandes@gmail.com>
2021-08-28 15:23:37 -0300

    done six

---
+++ challenges/desafio6.js
@@ -0,0 +1,21 @@
+db.movies.aggregate([
+  { $match: {
+    awards: { $regex: /^Won \d{1,2} Oscar/ },
+  } },
+  { $group: {
+    _id: null,
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    media_rating: { $avg: "$imdb.rating" },
+    desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+
+  } },
+  { $project: {
+    _id: false,
+    maior_rating: 1,
+    menor_rating: 1,
+    media_rating: { $round: ["$media_rating", 1] },
+    desvio_padrao: { $round: ["$desvio_padrao", 1] },
+
+  } },
+]);
