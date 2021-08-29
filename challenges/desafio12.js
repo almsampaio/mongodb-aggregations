6005609b7c4eb83204ea5a0657dc42392595b55a
ScriptCamilo <script.camilo@gmail.com>
2021-08-28 22:14:30 -0300

    feat: twelfth requirement done

---
+++ challenges/desafio12.js
@@ -0,0 +1,33 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      dayOfWeek: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $match: {
+      dayOfWeek: 5,
+    },
+  },
+  {
+    $group: {
+      _id: "$startStationName",
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "$total",
+    },
+  },
+]);
