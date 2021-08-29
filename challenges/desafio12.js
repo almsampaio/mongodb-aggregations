23c74c967b41f781a0ad666404d0dbbfceb0fc86
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-08-29 11:03:35 -0300

    fix:desafio12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,7 +1,7 @@
 db.trips.aggregate([
-    { $match: {$expr: {$eq: [{ $dayOfWeek: "$startTime" },5,],},} },
-    { $group: {_id: "$startStationName",count: { $sum: 1 },} },
-    { $sort: {count: -1,} },
-    { $limit: 1 },
-    { $project: {nomeEstacao: "$_id",total: "$count",_id: 0,} },
-  ]);package
+  { $match: { $expr: { $eq: [{ $dayOfWeek: "$startTime" }, 5] } } },
+  { $group: { _id: "$startStationName", count: { $sum: 1 } } },
+  { $sort: { count: -1 } },
+  { $limit: 1 },
+  { $project: { nomeEstacao: "$_id", total: "$count", _id: 0 } },
+]);
