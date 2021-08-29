f550ef4dd2a3edc9e6a1078c9678b8947aae8bd5
lmuffato <lmiremuffato@gmail.com>
2021-08-28 23:31:07 -0300

    requisito 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,5 +1,20 @@
 /* Desafio 14 - Baseado na duração média das viagens, determine quais
-
 são as 5 bicicletas que foram mais utilizadas.
-
-Exiba o resultado em minutos arredondados para cima e em ordem decrescente. */
+Arredonde o resultado para cima. */
+db.trips.aggregate([
+  { $match: { startTime: { $exists: true } } }, //
+  { $group:
+    {
+      _id: "$bikeid",
+      duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+    },
+  }, //
+  { $project:
+    {
+      duracaoMedia: { $ceil: { $divide: ["$duracaoMedia", 1000 * 60] } },
+    },
+  }, //
+  { $project: { _id: false, bikeid: "$_id", duracaoMedia: "$duracaoMedia" } },
+  { $sort: { duracaoMedia: -1 } },
+  { $limit: 5 },
+]);
