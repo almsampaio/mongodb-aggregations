ddcf232800367029cf8c056e755c58e69162e453
Perycles reis <perycles_reis@hotmail.com>
2021-08-29 15:50:46 -0300

    desafios 1 e 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,9 +1,3 @@
-/* eslint-disable max-len */
-// Desafio 2
-// A escolha do filme da noite foi um sucesso, mas infelizmente ficamos com nossa banda de internet quase esgotada, e ainda precisamos de uma nova recomendação de filme. Para diminuir o volume de dados trafegados:
-
-// Utilizando o mesmo pipeline anterior, retorne apenas os campos title, rated, imdb.rating, imdb.votes e year, modificando seus nomes para titulo, avaliado, notaIMDB, votosIMDB e ano, respectivamente.
-
 db.movies.aggregate([
   {
     $match: {
