214ea96039f4e0e498f27084c2d2faa6d41c0e5e
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-08-29 18:33:26 -0300

    missao 1 completa pt2

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -3,6 +3,6 @@ db.movies.aggregate([
     "imdb.rating": { $gte: 7 },
     genres: { $nin: ["Crime", "Horror"] },
     rated: { $in: ["PG", "G"] },
-    languages: {$all: ["English", "Spanish"]},
-  }},
+    languages: { $all: ["English", "Spanish"] },
+  } },
 ]);
