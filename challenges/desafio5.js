3c518e3444fbadac72470f7a356c98ce3b4242e9
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-29 16:57:14 -0300

    Feat: Desafio5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,7 +1,7 @@
 db.movies.aggregate([
   {
     $match: {
-      countries: "USA",
+      countries: { $in: ["USA"] },
       "tomatoes.viewer.rating": { $gte: 3 },
       cast: { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
     } },
@@ -14,10 +14,10 @@ db.movies.aggregate([
             ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"]] } },
     } },
   {
-    $project: { _id: 0, title: 1 },
+    $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
   },
   {
-    $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
+    $project: { _id: 0, title: 1 },
   },
   { $skip: 24 },
   { $limit: 1 },
