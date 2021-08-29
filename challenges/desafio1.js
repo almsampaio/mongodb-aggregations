91f4fab17fc3fb3dce6adc7a577c60525c221cd6
Perycles reis <perycles_reis@hotmail.com>
2021-08-29 15:48:59 -0300

    desafios 1 e 2

---
+++ challenges/desafio1.js
@@ -0,0 +1,21 @@
+// Desafio 1
+// Ajude a Trybe a escolher um filme para a próxima noite! Baseado em uma
+// pesquisa, decidimos que os filmes em potencial devem atender alguns critérios, vejamos:
+// Retorne todos os filmes que satisfaça, através de uma pipeline, as condições abaixo
+// imdb.rating deve ser maior ou igual a 7;
+// genres não deve conter Crime ou Horror;
+// rated deve ser igual a PG ou G;
+// languages contém English e Spanish.
+// Utilize a coleção movies.
+// Sua query deve retornar 41 documentos.
+
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $not: { $in: ["Crime", "Horror"] } },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+]);
