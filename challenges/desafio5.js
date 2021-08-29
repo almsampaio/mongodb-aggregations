34eb8c176333f5f193145a25aac8afb38f9c8b34
Antônio Schappo <antonioschappo@gmail.com>
2021-08-29 12:17:40 -0300

    first try challenge 6

---
+++ challenges/desafio5.js
@@ -0,0 +1,16 @@
+db.movies.aggregate([{
+  $addFields: { favorites: ["Sandra Bullock",
+    "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] } },
+{ $addFields: { num_favs: { $size: { $setIntersection: ["$cast", "$favorites"] } } } },
+{ $match: { cast: { $exists: true }, countries: { $in: ["USA"] }, "tomatoes.viewer.rating": { $gte: 3 } } },
+{
+  $sort: {
+    num_favs: -1, "tomatoes.viewer.rating": -1, title: -1,
+  } }, {
+  $project: {
+    _id: 0, title: 1,
+  } }, {
+  $skip: 24,
+}, {
+  $limit: 1,
+}]);
