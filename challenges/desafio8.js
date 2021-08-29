9be8428c3c886c451f5ea8800fa5b2377701f424
IzeldaValeriano <zel.valeriano@gmail.com>
2021-08-29 09:48:57 -0300

    requisitos 1 ao 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,32 @@
+db.air_routes.aggregate([
+    {
+      $match: {
+        airplane: { $in: ["747", "380"] },
+      },
+    },
+    {
+      $lookup: {
+        from: "air_alliances",
+        localField: "airline.name",
+        foreignField: "airlines",
+        as: "partnerships",
+      },
+    },
+    {
+      $unwind: "$partnerships",
+    },
+    {
+      $group: {
+        _id: "$partnerships.name",
+        totalRotas: { $sum: 1 },
+      },
+    },
+    {
+      $sort: {
+        totalRotas: -1,
+      },
+    },
+    {
+      $limit: 1,
+    },
+  ]);
