77a97685059e4c4c3793261cd000b7aa008f739c
Marina Alane <marina.alane@outlook.com>
2021-08-29 16:52:34 -0300

    desafio 9 ok

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -7,18 +7,22 @@
 
 db.trips.aggregate([
   { $match: {
-    $and: [
-      { birthYear: { $exists: true } },
-      { birthYear: { $ne: "" } },
-    ],
+    birthYear: { $exists: true, $ne: "" },
   } },
   { $group: {
     _id: null,
-    maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
-    menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+    maiorAnoNascimento: {
+      $max: {
+        $toInt: "$birthYear",
+      },
+    },
+    menorAnoNascimento: {
+      $min: {
+        $toInt: "$birthYear",
+      },
+    },
   } },
   { $project: {
-    maiorAnoNascimento: 1,
-    menorAnoNascimento: 1,
+    _id: 0,
   } },
 ]);
