18b699901665e920c2be3d999ba63abb4bb9dd0d
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-08-30 12:31:37 -0400

    fix: desafio 7 - line 20

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -17,11 +17,11 @@ db.movies.aggregate([
     },
   },
   {
-    $sort: { numeroFilmes: -1, "_id.cast": -1 },
+    $sort: { numeroFilmes: -1, _id: -1 },
   },
   {
     $project: {
-      _id: 1,
+      // _id: 1,
       numeroFilmes: 1,
       mediaIMDB: { $round: ["$mediaIMDB", 1] },
     },
