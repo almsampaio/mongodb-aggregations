0947b7b13ef2796dc427b00671b01e044d6deaaf
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-29 21:03:06 -0300

    Fix: Desafio14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,7 +1,7 @@
 db.trips.aggregate([
   { $addFields: { duracaoViagem: { $subtract: ["$stopTime", "$startTime"] } } },
   { $group: { _id: "$bikeid", duracaoMediaViagem: { $avg: "$duracaoViagem" } } },
-  { $project: { bikeId: "$_id", duracaoMedia: { $ceil: { $divide: ["$duracaoMediaViagem", 60000] } } } },
+  { $project: { bikeId: "$_id", _id: 0, duracaoMedia: { $ceil: { $divide: ["$duracaoMediaViagem", 60000] } } } },
   { $sort: { duracaoMedia: -1 } },
   { $limit: 5 },
 ]);
