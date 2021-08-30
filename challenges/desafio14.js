938e44b8df1d6c75c246108eb578f933f558bdb0
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-08-30 13:56:28 -0400

    feat: desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,30 @@
+// use("aggregations");
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] }, 60000,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $sort: { duracaoMedia: -1 },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeid: "$_id",
+      duracaoMedia: {
+        $ceil: "$duracaoMedia",
+      },
+    },
+  },
+  {
+    $limit: 5,
+  },
+]);
