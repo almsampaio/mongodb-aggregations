92e06ad7ce2090b9d7aedf4d11b703e3086f3b84
Sergio Martins <sergiomos2010@gmail.com>
2021-08-30 09:55:07 -0300

    Feat: desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,33 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $match: {
+      diaDaSemana: 5,
+    },
+  },
+  {
+    $group: {
+      _id: "$startStationName",
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "$total",
+    },
+  },
+]);
