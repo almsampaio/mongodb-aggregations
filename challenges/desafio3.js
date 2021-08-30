1b6c670390c61a88667e5c40d840e3cc13d11d56
deltadigo <deltadigo@gmail.com>
2021-08-30 09:33:24 -0300

    Requisito 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,8 @@
+db.movies.aggregate([
+  { $match: { "imdb.rating": { $gte: 7 } } },
+  { $match: { genres: { $nin: ["Crime", "Horror"] } } },
+  { $match: { rated: { $in: ["PG", "G"] } } },
+  { $match: { languages: { $all: ["English", "Spanish"] } } },
+  { $project: { _id: 0, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" } },
+  { $sort: { ano: -1, notaIMDB: -1, titulo: 1 } },
+]);
