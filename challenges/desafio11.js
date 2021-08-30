0ecebf1ef82f46efdd958ae3d980fe7f209d2c03
deltadigo <deltadigo@gmail.com>
2021-08-30 14:41:30 -0300

    COrreções

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,7 +1,23 @@
 db.trips.aggregate([
-  { $addFields: { dayOfWeek: { $dayOfWeek: "$startTime" } } },
-  { $group: { _id: "$dayOfWeek", count: { $sum: 1 } } },
-  { $sort: { count: -1 } },
-  { $limit: 1 },
-  { $project: { _id: 0, diaDaSemana: "$_id", total: "$count" } },
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
 ]);
