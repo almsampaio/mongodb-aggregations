ef6d29e77a6971cf3d501a8392feb941874704f5
Lucas Pedroso <lepsk8@gmail.com>
2021-08-30 17:20:10 -0300

    desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,32 @@
+const MINUTE_IN_MILLISECONDS = 1000 * 60;
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            MINUTE_IN_MILLISECONDS,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMedia" },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+]);
