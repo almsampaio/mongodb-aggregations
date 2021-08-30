6e7af2fd97fc9683b11f244514960c3d17fd511b
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-08-30 11:01:57 -0400

    fix: desafio 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,4 +1,29 @@
-use("aggregations");
+// use("aggregations");
 db.movies.aggregate([
-  
+  {
+    $match: {
+      languages: {
+        $in: ["English"] },
+    },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $sort: { numeroFilmes: -1, "_id.cast": -1 },
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
 ]);
