e230ba0484511da395d0dedaba6906ffa63b393a
Lucas Pedroso <lepsk8@gmail.com>
2021-08-30 17:19:59 -0300

    desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,31 @@
+const MINUTE_IN_MILLISECONDS = 1000 * 60;
+
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00Z"),
+        $lt: ISODate("2016-04-10T00:00:00Z"),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            MINUTE_IN_MILLISECONDS,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
