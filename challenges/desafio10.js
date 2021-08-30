9c04e950155126d869ef17f4202d579ac2b3c1dd
Luiz <luizfvaccarif@gmail.com>
2021-08-30 16:52:12 -0300

    Fix: lint challenge 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,7 +2,7 @@ db.trips.aggregate([
   {
     $group: {
       _id: "$usertype",
-      "media": { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] } },
+      media: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] } },
     },
   },
   {
