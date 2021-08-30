4d03f2dc79680345892812bf802f75cf968bf646
Luiz <luizfvaccarif@gmail.com>
2021-08-30 15:13:43 -0300

    Feat: challenge 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,18 @@
+db.trips.aggregate([
+  {
+    $match: {
+      $and: [{ startTime: { $gte: ISODate("2016-03-10") } }, { startTime: { $lt: ISODate("2016-03-11") } }] },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000] } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
