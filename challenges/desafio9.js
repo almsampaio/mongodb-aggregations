e7732b428a2b819ac10f41516ab0313bfc2d6206
deltadigo <deltadigo@gmail.com>
2021-08-30 10:52:34 -0300

    Requisito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: { $exists: true, $ne: "" },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: "$birthYear" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+    },
+  },
+]);
