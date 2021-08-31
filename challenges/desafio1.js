2e54d3099a70cce2b5d21fcb8d4f7889dc06f191
Junior Portela <portelajr14@gmail.com>
2021-08-30 22:31:15 -0300

    fix: desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,12 +1,15 @@
-db.movies.aggregate([
-  {
-    $match: {
-      $and: [
-        { "imdb.rating": { $gte: 7 } },
-        { genres: { $nin: ["Crime", "Horror"] } },
-        { rated: { $in: ["PG", "G"] } },
-        { languages: { $all: ["English", "Spanish"] } },
-      ],
+db.movies.aggregate(
+  [
+    {
+      $match: {
+        $and:
+        [
+          { "imdb.rating": { $gte: 7 } },
+          { genres: { $nin: ["Crime", "Horror"] } },
+          { rated: { $in: ["PG", "G"] } },
+          { languages: { $all: ["English", "Spanish"] } },
+        ],
+      },
     },
-  },
-]);
+  ],
+);
