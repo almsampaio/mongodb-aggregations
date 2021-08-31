349e478b549bc416096a8a4dcb37b106cb0f7213
Perycles reis <perycles_reis@hotmail.com>
2021-08-31 11:04:30 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,37 @@
+const pessoas = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
+db.movies.aggregate([
+  {
+    $match: {
+      cast: { $in: pessoas },
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: [pessoas, "$cast"],
+        },
+      },
+    },
+  },
+  {
+    $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
+  },
+  {
+    $project: {
+      title: 1,
+      _id: 0,
+    },
+  },
+  { $skip: 24 },
+  { $limit: 1 },
+]);
