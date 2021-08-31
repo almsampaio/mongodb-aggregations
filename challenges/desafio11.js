9e6e9eedb3fbaf58154a7873d54ee7b5b030d0e2
lucas-chave <lcchavesr150@gmail.com>
2021-08-31 13:46:17 -0300

    fix challenge 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,9 +1,10 @@
 // challenge 11;
+// https://github.com/tryber/sd-010-a-mongodb-aggregations/commit/6c260a6043c2df9e1f1427f8abbaab15ac2e4b55
 
 db.trips.aggregate(
   [
     { $group: { _id: { $dayOfWeek: "$startTime" }, total: { $sum: 1 } } },
-    { $project: { _id: 0, diaDaSemana: "$_id", total: 1 } },
+    { $project: { _id: 0, diaDaSemana: "$_id", total: "$total" } },
     { $sort: { total: -1 } }, { $limit: 1 },
   ],
 );
