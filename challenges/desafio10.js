076a5bc8f4e89d655c30ba151328cc565fde9157
lucas-chave <lcchavesr150@gmail.com>
2021-08-31 10:22:16 -0300

    challenge 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,8 @@
+// challenge 10;
+
+db.trips.aggregate(
+  [
+    { $group: { _id: "$usertype", duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } },
+    { $project: { tipo: "$_id", duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2] }, _id: 0 } },
+  ],
+);
