6ddee7965c2fb82a45532fba972e312fc5060cd1
lucas-chave <lcchavesr150@gmail.com>
2021-08-31 22:49:57 -0300

    fix challenge 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -7,6 +7,7 @@ db.trips.aggregate(
     { $match: { diaDaSemana: 5 } },
     { $group: { _id: "$startStationName", total: { $sum: 1 } } },
     { $project: { nomeEstacao: "$_id", total: "$total", _id: 0 } },
-    { $sort: { total: -1 } }, { $limit: 1 },
+    { $sort: { total: -1 } },
+    { $limit: 1 },
   ],
 );
