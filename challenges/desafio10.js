be005b272d9e742ac42b3e59a6d70bb56d6f8058
lucas-chave <lcchavesr150@gmail.com>
2021-09-01 09:31:27 -0300

    fix challenge 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -4,5 +4,6 @@ db.trips.aggregate(
   [
     { $group: { _id: "$usertype", duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } },
     { $project: { tipo: "$_id", duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2] }, _id: 0 } },
+    { $sort: { duracaoMedia: 1 } },
   ],
 );
