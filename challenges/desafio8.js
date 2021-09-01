d633fb67308d943f519c5a02d0cd48eadfbd0c09
vinicios <soicinivt@gmail.com>
2021-09-01 16:54:33 -0300

    Desafio 8. with ctrl + s "--

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,7 +1,117 @@
-db.air_routes.aggregate([
+// db.air_routes.aggregate([
+//   {
+//     $match: {
+//       airplane: {
+//         $in: [
+//           "747",
+//           "380",
+//         ],
+//       },
+//     },
+//   },
+//   {
+//     $group: {
+//       _id: "$airline.name",
+//       numero_de_sla: {
+//         $sum: 1,
+//       },
+//     },
+//   },
+//   {
+//     $sort: {
+//       _id: 1,
+//     },
+//   },
+// ]).pretty();
+
+// db.air_alliances.aggregate([
+//   {
+//     $unwind: "$airlines",
+//   },
+//   {
+//     $lookup:
+//     {
+//       from: "air_routes",
+//       let: { airlines2: "$airlines", name2: "$name" },
+//       pipeline: [
+//         {
+//           $match: {
+//             airplane: {
+//               $in: [
+//                 "747",
+//                 "380",
+//               ],
+//             },
+//           },
+//         },
+//         {
+//           $match: {
+//             $expr: {
+//               $eq: ["$airline.name", "$$airlines2"],
+//             },
+//           },
+//         },
+//         {
+//           $group: {
+//             _id: "$airline.name",
+//             numero_de_sla: {
+//               $sum: 1,
+//             },
+//           },
+//         },
+//         {
+//           $group: {
+//             _id: "$$name2",
+//             count: { $sum: "$numero_de_sla" },
+//           },
+//         },
+//         {
+//           $match: {
+//             _id: "SkyTeam",
+//           },
+//         },
+//       ],
+//       as: "teste",
+//     },
+//   },
+//   {
+//     $project: {
+//       _id: 0,
+//     },
+//   },
+//   {
+//     $sort: {
+//       "teste.count": -1,
+//     },
+//   },
+//   {
+//     $limit: 1,
+//   },
+// ]);
+
+// db.air_alliances.findOne();
+
+// db.air_routes.findOne();
+
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup:
+    {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "teste",
+    },
+  },
+  {
+    $unwind: "$teste",
+  },
   {
     $match: {
-      airplane: {
+      "teste.airplane": {
         $in: [
           "747",
           "380",
@@ -11,43 +121,16 @@ db.air_routes.aggregate([
   },
   {
     $group: {
-      _id: "$airline.name",
+      _id: "$name",
+      totalRotas: { $sum: 1 },
     },
   },
   {
     $sort: {
-      _id: 1,
+      "teste.totalRotas": -1,
     },
   },
-  // {
-  //   $project: {
-  //     "airline.name": 1,
-  //   },
-  // },
-]).pretty();
-
-db.air_alliances.aggregate([
   {
-    $unwind: "$airlines",
-  },
-  {
-    $lookup:
-      {
-        from: "air_routes",
-        let: { airlines2: "$airlines", name2: "$name" },
-        pipeline: [
-          { $match:
-              { $expr:
-                    [
-                      { $eq: ["$airline.name", "$$airlines2"] },
-                    ],
-              },
-          },
-        ],
-      },
+    $limit: 1,
   },
 ]);
-
-db.air_alliances.findOne();
-
-db.air_routes.findOne();
