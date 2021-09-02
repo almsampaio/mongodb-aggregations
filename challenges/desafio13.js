15b311f0a3784ecd1a4fb7a4df02ec522eb0ee71
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-09-01 23:46:02 -0300

    se fosse um apocalipse de verdade geral tinha morrido por causa do lint

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -2,7 +2,7 @@ db.trips.aggregate([
   { $match: {
     startTime: {
       $gte: ISODate("2016-03-10"),
-      $lt: ISODate("2016-03-11")
+      $lt: ISODate("2016-03-11"),
     },
   } },
   { $group: {
