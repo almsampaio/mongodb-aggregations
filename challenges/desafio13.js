0f624899b1bbf564c2918f93f6f80bd6c62366f9
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-09-01 23:43:01 -0300

    apocalipse cancelado

---
+++ challenges/desafio13.js
@@ -0,0 +1,24 @@
+db.trips.aggregate([
+  { $match: {
+    startTime: {
+      $gte: ISODate("2016-03-10"),
+      $lt: ISODate("2016-03-11")
+    },
+  } },
+  { $group: {
+    _id: null,
+    average: {
+      $avg: {
+        $subtract: ["$stopTime", "$startTime"],
+      },
+    },
+  } },
+  { $project: {
+    _id: 0,
+    duracaoMediaEmMinutos: {
+      $ceil: {
+        $divide: ["$average", 60000],
+      },
+    },
+  } },
+]);
