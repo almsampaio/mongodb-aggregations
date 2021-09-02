375dd1190b29be8311fe31bb8fabe4c978bcbab3
Guilherme de Prais <guilherme.prais@gmail.com>
2021-09-02 09:22:50 -0300

    req 05

---
+++ challenges/desafio5.js
@@ -0,0 +1,33 @@
+const atoresFavoritos = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": {
+        $gte: 3,
+      },
+      cast: {
+        $exists: true,
+      },
+    },
+  },
+  {
+    $addFields: {
+      actors: {
+        $setIntersection: [atoresFavoritos, "$cast"],
+      },
+    },
+  },
+  { $addFields: { num_favs: { $size: "$actors" } } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $project: { _id: 0, title: 1 } },
+  { $skip: 24 },
+  { $limit: 1 },
+]);
