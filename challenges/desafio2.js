f42fcd4637bd25a02ec320fc691fbda3682f01c1
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-09-03 17:44:31 -0300

    desafio 3

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -9,12 +9,12 @@ db.movies.aggregate([
   },
   {
     $project: {
+      _id: 0,
       titulo: "$title",
       avaliado: "$rated",
       notaIMDB: "$imdb.rating",
       votosIMDB: "$imdb.votes",
       ano: "$year",
-      _id: 0,
     },
   },
 ]);
