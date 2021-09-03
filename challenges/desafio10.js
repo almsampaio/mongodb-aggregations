6966f9e61a4dceca3975192f1f60c7bdfb4f9743
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-09-03 19:44:46 -0300

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,22 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+      _id: 0,
+    },
+  },
+  {
+    $sort: { duracaoMedia: 1 },
+  },
+]);
