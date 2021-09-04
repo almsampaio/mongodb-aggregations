540be10f395a7ff6e5e71e81788acbf97befd60e
Southier <patrick.southier@hotmail.com>
2021-09-04 05:56:58 -0300

    feat: completes requirements 5 and 6

---
+++ challenges/desafio5.js
@@ -0,0 +1,34 @@
+db.movies.aggregate([
+  { $match: {
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+    cast: { $exists: true },
+  } },
+  { $addFields: {
+    num_favs: {
+      $let: {
+        vars: {
+          favs: { $setIntersection: [[
+            "Sandra Bullock",
+            "Tom Hanks",
+            "Julia Roberts",
+            "Kevin Spacey",
+            "George Clooney",
+          ], "$cast"] },
+        },
+        in: { $size: "$$favs" },
+      },
+    },
+  } },
+  { $sort: {
+    num_favs: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
+  } },
+  { $project: {
+    _id: 0,
+    title: 1,
+  } },
+  { $skip: 24 },
+  { $limit: 1 },
+]);
