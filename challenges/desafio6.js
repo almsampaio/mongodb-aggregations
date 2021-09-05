42d842aada80c3ba46d6c63acf1c6b067809869c
Maurice Alves <maurice.estevam@gmail.com>
2021-09-05 13:02:26 -0300

    Requisito - 6.

---
+++ challenges/desafio6.js
@@ -0,0 +1,25 @@
+db.movies.aggregate([
+  {
+    $match: {
+      awards: { $regex: /Won.*Oscars/ },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_rating: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_rating: { $round: ["$desvio_rating", 1] },
+    },
+  },
+]);
