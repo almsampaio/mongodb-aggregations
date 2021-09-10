ec94a7de1abd22208d447fd016f168a07321fbfd
jepaur5 <jepaur5@gmail.com>
2021-09-10 16:31:05 -0300

    requisito5

---
+++ challenges/desafio5.js
@@ -0,0 +1,41 @@
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $exists: true },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: ["$cast",
+            [
+              "Sandra Bullock",
+              "Tom Hanks",
+              "Julia Roberts",
+              "Kevin Spacey",
+              "George Clooney",
+            ],
+          ],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  { $skip: 24 },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+  { $limit: 1 },
+]);
