7c71b8f2a84aceb8086324140b405dd116054d52
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-09-10 18:16:58 -0300

    Req 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,20 @@
+const Hour = 3600000;
+
+db.trips.aggregate([
+  { $group: {
+    _id: "$usertype",
+    contador: {
+      $avg: { $subtract: ["$stopTime", "$startTime"] },
+    },
+  } },
+  { $sort: {
+    contador: 1,
+  } },
+  { $project: {
+    tipo: "$_id",
+    duracaoMedia: {
+      $round: [{ $divide: ["$contador", Hour] }, 2],
+    },
+    _id: 0,
+  } },
+]);
