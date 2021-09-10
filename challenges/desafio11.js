3404bb1b9eac11f759a28de6bda6f79e576c44e3
MuriloBatista <mub4si@gmail.com>
2021-09-10 08:49:51 -0300

    desafio10, 11 e 12

---
+++ challenges/desafio11.js
@@ -0,0 +1,20 @@
+db.trips.aggregate([
+  { $addFields: {
+    diaDaPartida: {
+      $dayOfWeek: "$startTime",
+    },
+  } },
+  { $group: {
+    _id: "$diaDaPartida",
+    totalViagens: { $sum: 1 },
+  } },
+  { $project: {
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: "$totalViagens",
+  } },
+  { $sort: {
+    total: -1,
+  } },
+  { $limit: 1 },
+]);
