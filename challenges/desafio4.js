bc11d3632fd440e386add0357a296a1fc4f34b86
IzeldaValeriano <zel.valeriano@gmail.com>
2021-09-12 09:43:29 -0300

    corrigindo formataçao

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,11 +1,11 @@
 db.movies.aggregate([
-    {
-      $addFields: { title_split: { $split: ['$title', ' '] } } },
-    {
-      $match: { title_split: { $size: 1 } },
-    },
-    { $sort: { title: 1 } },
-    {
-      $project: { _id: 0, title_split: 1 },
-    },
-  ]);
+  {
+    $addFields: { title_split: { $split: ["$title", " "] } } },
+  {
+    $match: { title_split: { $size: 1 } },
+  },
+  { $sort: { title: 1 } },
+  {
+    $project: { _id: 0, title_split: 1 },
+  },
+]);
