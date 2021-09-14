fcb291dd133c348bfa184a4693b628d56bacd48b
Antônio Schappo <antonioschappo@gmail.com>
2021-09-14 14:30:02 -0300

    correct challenge 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -11,17 +11,5 @@ db.trips.aggregate([{
   $project: {
     _id: 0,
     diaDaSemana: "$_id",
-    total: 1 } },
+    total: "$total" } },
 { $sort: { total: -1 } }, { $limit: 1 }]);
-
-// ### Desafio 11
-
-// #### Determine qual o dia da semana com maior número de viagens iniciadas.
-
-// **Dica:** Utilize o operador [`$dayOfWeek`](https://docs.mongodb.com/manual/reference/operator/aggregation/dayOfWeek/index.html)
-//  para extrair o dia da semana como um número de uma data.
-
-// O resultado da sua query deve ter exatamente o seguinte formato (incluindo a ordem dos campos):
-
-// ```javascript
-// { "diaDaSemana" : <dia_da_semana>, "total" : <total_de_viagens> }
