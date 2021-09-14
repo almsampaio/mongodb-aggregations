b7d32773d3e073bd2c4e35983aee7ab6ac7c8bd1
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-09-13 22:51:34 -0300

    feat: req13

---
+++ challenges/desafio13.js
@@ -0,0 +1,25 @@
+db.trips.aggregate([
+  { $match: {
+    startTime: { $gte: ISODate("2016-03-10") },
+  } },
+  { $group: {
+    _id: null,
+    travelTime: {
+      $avg: {
+        $dateDiff: {
+          startDate: "$startTime",
+          endDate: "$stopTime",
+          unit: "minute",
+        },
+      } },
+  } },
+  { $project: {
+    duracaoMediaEmMinutos: { $ceil: "$travelTime" },
+    _id: 0,
+  } },
+]);
+
+// Referências:
+// DateDiff: https://docs.mongodb.com/manual/reference/operator/aggregation/dateDiff/#mongodb-expression-exp.-dateDiff
+// ... "The $dateDiff expression returns a time difference expressed in integer units" ...
+// ISODate: https://docs.mongodb.com/manual/reference/method/Date/
