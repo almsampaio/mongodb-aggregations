e24a1825b92dcb6951dc032ee00c792d3bec6e86
jlfagundes <jlfagundes@gmail.com>
2021-09-14 12:36:21 -0300

    feat: challenge 12 done using $group, $dayOfWeek, $sum, $sort, $project and $limit

---
+++ challenges/desafio12.js
@@ -0,0 +1,29 @@
+use('aggregations');
+
+db.trips.aggregate(
+  {
+    $group: {
+      _id: {
+        nomeEstacao: "$startStationName",
+        diaDaSemana: { $dayOfWeek: "$startTime" },
+      },
+      total: { $sum: 1 }
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $project: {
+      nomeEstacao: "$_id.nomeEstacao",
+      total: "$total",
+      _id: 0
+    },
+  },
+  {
+    $limit: 1,
+  },
+);
+
