4fe34da6239e5c59680cd7f9b4526c9e801d4f02
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-14 12:18:58 -0400

    feat: query that counts selected casts from 24th document

---
+++ challenges/desafio5.js
@@ -0,0 +1,22 @@
+const selectedCast = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+use("aggregations");
+db.movies.aggregate([
+  { $match: {
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+  } },
+  { $addFields: {
+    num_favs: {
+      $cond: {
+        if: { $isArray: "$cast" },
+        then: { $size: {
+          $setIntersection: ["$cast", selectedCast],
+        } },
+        else: "$$REMOVE",
+      },
+    } } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $skip: 24 },
+  { $limit: 1 },
+  { $project: { _id: 0, title: 1 } },
+]);
