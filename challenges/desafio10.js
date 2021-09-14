57b65bda1876faefa2de83d0ae0b1dd1428064fc
jlfagundes <jlfagundes@gmail.com>
2021-09-13 21:24:28 -0300

    feat: challenge 10 done using $match, $avg, $subtract, $project, $divide, $round and $sort.

---
+++ challenges/desafio10.js
@@ -0,0 +1,29 @@
+//Solução encontrada com ajuda de Eduardo Costa - Turma 10A
+db.trips.aggregate(
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [
+          { $divide: ["$duracaoMedia", 3.6e+6] }, 2,
+        ],
+      },
+      _id: 0,
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: 1,
+    }
+  }
+);
