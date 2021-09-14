33d244a944c9671a88ffa6b0a6064da5cbb9f239
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-14 17:18:10 -0400

    feat: query that returns max, min, average and standart deviation from rating on movies that won oscars

---
+++ challenges/desafio6.js
@@ -0,0 +1,17 @@
+db.movies.aggregate([
+  { $match: { awards: { $regex: /won\s\d+\soscar/i } } },
+  { $group: {
+    _id: "junkook nova contratação do vasco",
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    media_rating: { $avg: "$imdb.rating" },
+    desvio_rating: { $stdDevSamp: "$imdb.rating" },
+  } },
+  { $project: {
+    _id: 0,
+    maior_rating: 1,
+    menor_rating: 1,
+    media_rating: { $round: ["$media_rating", 1] },
+    desvio_rating: { $round: ["$desvio_rating", 1] },
+  } },
+]);
