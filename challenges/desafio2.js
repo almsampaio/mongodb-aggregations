5aaf8e045dd6d6d470abc9a7f20559d748bc08e9
Ederson S <edersonrodrigo31@gmail.com>
2021-09-15 20:20:11 -0300

    feat: requisito 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,22 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } },
+      ],
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+]);
