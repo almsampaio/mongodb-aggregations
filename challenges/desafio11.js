8603ea936bed9dce34eee20cd02da1a1b7d807c7
Renan Costa <renancosta21@gmail.com>
2021-09-15 16:09:00 -0300

    feat: req 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      day: {
+        $dayOfWeek: "$startTime",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$day",
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+  {
+    $sort: { total: -1 },
+  },
+  { $limit: 1 },
+]);
