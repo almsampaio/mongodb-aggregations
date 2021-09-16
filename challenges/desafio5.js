9a863b363a92b641a06e128f40721b8914584ea0
Junior Portela <portelajr14@gmail.com>
2021-09-15 23:17:47 -0300

    desafio5: documento 25 que possui atores determinados  no cast

---
+++ challenges/desafio5.js
@@ -0,0 +1,37 @@
+const favActors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+db.movies.aggregate(
+  [
+    {
+      $match: {
+        countries: "USA",
+        "tomatoes.viewer.rating": { $gte: 3 },
+        cast: { $exists: 1 },
+      },
+    },
+    {
+      $addFields: {
+        num_favs: { $size: { $setIntersection: [favActors, "$cast"] } },
+      },
+    },
+    {
+      $sort: {
+        num_favs: -1,
+        "tomatoes.viewer.rating": -1,
+        title: -1,
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        title: 1,
+        num_favs: 1,
+      },
+    },
+    {
+      $skip: 24,
+    },
+    {
+      $limit: 1,
+    },
+  ],
+);
