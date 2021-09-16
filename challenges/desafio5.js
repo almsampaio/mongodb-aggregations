9ebe4e4af2ae7260d2b90ddff0a59cf7a373a6c8
nyxnil <nsmnilsonn@gmail.com>
2021-09-15 23:06:57 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,24 @@
+const actoFav = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+db.movies.aggregate([
+  { $project: { _id: 0, title: 1 } },
+  { $skip: 24 },
+  { $match: {
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+  } },
+  { $addFields: {
+    num_fav: { $size: { $setIntersection: [actoFav, "$cast"] } },
+  } },
+  { $sort: {
+    num_fav: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
+  } },
+  { $limit: 1 },
+]);
