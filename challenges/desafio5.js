252a9aa1ed6e9ba18c51a933cfbb93563ff38615
nyxnil <nsmnilsonn@gmail.com>
2021-09-15 23:58:43 -0300

    desafio 5 refatorado, posicao das chaves de agregacao, desafio 6.

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -6,8 +6,6 @@ const actoFav = [
   "George Clooney",
 ];
 db.movies.aggregate([
-  { $project: { _id: 0, title: 1 } },
-  { $skip: 24 },
   { $match: {
     countries: "USA",
     "tomatoes.viewer.rating": { $gte: 3 },
@@ -21,5 +19,7 @@ db.movies.aggregate([
     "tomatoes.viewer.rating": -1,
     title: -1,
   } },
+  { $project: { _id: 0, title: 1 } },
+  { $skip: 24 },
   { $limit: 1 },
 ]);
