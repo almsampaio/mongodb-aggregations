6801057e369baf1208f1c0327ee126bbf2e1c968
Marcos Albuquerque <mpdsa20@gmail.com>
2021-09-16 10:48:50 -0300

    Desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,39 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      horas: {
+        $divide: [
+          {
+            $subtract: [
+              "$stopTime",
+              "$startTime",
+            ],
+          },
+          3600000,
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      mediaHoras: {
+        $avg: "$horas",
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: ["$mediaHoras", 2],
+      },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: 1,
+    },
+  },
+]);
