299cdb57cbf4abd49342838075f72d12be9ca228
Paulo Henrique <ph.pereira.7@gmail.com>
2021-09-16 05:56:41 -0300

    Desafio 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,5 +1,12 @@
 db.movies.aggregate([
   { $match: { cast: { $exists: true } } },
+  { $match: { languages: { $elemMatch: { $eq: "English" } } } },
   { $unwind: "$cast" },
-  { $project: { cast: 1 } },
+  { $group: {
+    _id: "$cast",
+    numeroFilmes: { $sum: 1 },
+    mediaIMDB: { $avg: "$imdb.rating" },
+  } },
+  { $sort: { numeroFilmes: -1, _id: 1 } },
+  { $project: { numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
 ]);
