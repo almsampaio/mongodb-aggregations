225b631e2a9682b2ee4d4d89e5ab80f8c0a20e93
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-16 04:02:48 -0400

    fix: removing limit and changing from in to all

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -3,7 +3,6 @@ db.movies.aggregate([
       { "imdb.rating": { $gte: 7 },
         genres: { $nin: ["Crime", "Horror"] },
         rated: { $in: ["PG", "G"] },
-        languages: { $in: ["English", "Spanish"] },
+        languages: { $all: ["English", "Spanish"] },
       } },
-  { $limit: 41 },
 ]);
