812c02e6360f7dc1a1cc50b2a07c766c463cdc13
pauloeliezerg <pauloeliezerg@gmail.com>
2021-09-16 04:51:57 -0300

    req06

---
+++ challenges/desafio6.js
@@ -0,0 +1,21 @@
+const oscarCheck = /^Won \d{1,2} Oscar/;
+
+db.movies.aggregate([
+  { $match: {
+    awards: { $regex: oscarCheck },
+  } },
+  { $group: {
+    _id: null,
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    media_rating: { $avg: "$imdb.rating" },
+    desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+  } },
+  { $project: {
+    maior_rating: 1,
+    menor_rating: 1,
+    media_rating: { $round: ["$media_rating", 1] },
+    desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    _id: 0,
+  } },
+]);
