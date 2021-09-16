5d7cc97f78c7a6d195c54edc759c4b3ae8e0e90d
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-16 00:27:22 -0400

    feat: query that counts the routes in alliances with planes 747 and 380

---
+++ challenges/desafio8.js
@@ -0,0 +1,15 @@
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  { $lookup: {
+    from: "air_routes",
+    localField: "airlines",
+    foreignField: "airline.name",
+    as: "matches",
+  } },
+  { $unwind: "$matches" },
+  { $match: { "matches.airplane": { $in: ["747", "380"] } } },
+  { $group: {
+    _id: "$name",
+    totalRotas: { $sum: 1 },
+  } },
+]);
