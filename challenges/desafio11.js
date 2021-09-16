3eb1b0a08b9afcdd5835e591b8f208c2e19e7544
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-16 02:01:40 -0400

    feat: day of week that had most travels

---
+++ challenges/desafio11.js
@@ -0,0 +1,14 @@
+use("aggregations");
+db.trips.aggregate([
+  { $group: {
+    _id: { $dayOfWeek: "$startTime" },
+    totalSum: { $sum: 1 },
+  } },
+  { $project: {
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: "$totalSum",
+  } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
