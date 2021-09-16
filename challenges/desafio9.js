43a7c29b5bc94bc04bdaec00bdde863d784cfc8a
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-16 00:50:30 -0400

    fix: eslint issues

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,10 +1,10 @@
 db.trips.aggregate([
-  { $match: { birthYear: { $ne: "" } }
+  { $match: { birthYear: { $ne: "" } },
   },
   { $group: {
     _id: "vasco",
     maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
     menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
   } },
-  { $project: { _id: 0, maiorAnoNascimento: 1, menorAnoNascimento: 1 } }
+  { $project: { _id: 0, maiorAnoNascimento: 1, menorAnoNascimento: 1 } },
 ]);
