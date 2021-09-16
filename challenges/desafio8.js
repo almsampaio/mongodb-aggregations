88980c10ccf8fcb2110c2ec0e74babeca148c6f8
Andre Sampaio <andresampaiolm@gmail.com>
2021-09-15 21:52:23 -0300

    REQ 08

---
+++ challenges/desafio8.js
@@ -0,0 +1,32 @@
+db.air_alliances.aggregate([
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "rotas",
+    },
+  },
+  {
+    $unwind: "$rotas",
+  },
+  {
+    $match: {
+      "rotas.airplane": { $in: ["747", "380"] },
+    },
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
