ce8df2d3137cc06137a0f12f3e740e767a1d39d8
Junior Portela <portelajr14@gmail.com>
2021-09-15 22:48:08 -0300

    desafio 4: split da string titulo e projeçao size 1

---
+++ challenges/desafio4.js
@@ -0,0 +1,31 @@
+db.movies.aggregate(
+  [
+    {
+      $addFields: {
+        title_split: { $split: ["$title", " "] },
+      },
+    },
+    {
+      $match: {
+        title_split: { $size: 1 },
+      },
+    },
+    {
+      $sort: {
+        titulo: 1,
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        title_split: 1,
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        docs: { $sum: 1 },
+      },
+    },
+  ],
+);
