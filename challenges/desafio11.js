5f5004df582ffbfd01d9bf3f795b173e7ea4eec0
nyxnil <nsmnilsonn@gmail.com>
2021-09-16 08:06:35 -0300

    desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,12 @@
+db.trips.aggregate([
+  { $group: {
+    _id: { $dayOfWeek: "$startTime" },
+    qntViagens: { $sum: 1 },
+  } },
+  { $limit: 1 },
+  { $project: {
+    diaDaSemana: "$_id",
+    total: "$qntViagens",
+    _id: 0,
+  } },
+]);
