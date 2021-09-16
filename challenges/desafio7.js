091953751c81cf30a4eb45d8061f2783a35e872c
Junior Portela <portelajr14@gmail.com>
2021-09-16 00:06:33 -0300

    desafio7: identificar e ordenar filmes por ator

---
+++ challenges/desafio7.js
@@ -0,0 +1,31 @@
+db.movies.aggregate(
+  [
+    {
+      $unwind: "$cast",
+    },
+    {
+      $match: {
+        languages: "English",
+      },
+    },
+    {
+      $group: {
+        _id: "$cast",
+        numeroFilmes: { $sum: 1 },
+        mediaIMDB: { $avg: "$imdb.rating" },
+      },
+    },
+    {
+      $project: {
+        numeroFilmes: 1,
+        mediaIMDB: { $round: ["$mediaIMDB", 1] },
+      },
+    },
+    {
+      $sort: {
+        numeroFilmes: -1,
+        _id: -1,
+      },
+    },
+  ],
+);
