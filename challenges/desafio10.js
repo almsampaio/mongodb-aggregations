cb9a1dc69f56d8cc6eeed1df7c64e717e5a25bb7
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-16 01:47:12 -0400

    feat: query that shows trip duration of each type of user

---
+++ challenges/desafio10.js
@@ -0,0 +1,18 @@
+use("aggregations");
+db.trips.aggregate([
+  {
+    $addFields: {
+      duration: { $divide: [
+        { $subtract: ["$stopTime", "$startTime"] }, 3600000,
+      ] },
+    } },
+  { $group: {
+    _id: "$usertype",
+    duration: { $avg: "$duration" },
+  } },
+  { $project: {
+    _id: 0,
+    tipo: "$_id",
+    duracaoMedia: { $round: ["$duration", 2] },
+  } },
+]);
