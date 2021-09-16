a100eb7ac7899dc8b2c324a10ec39cc97a473e27
Ederson S <edersonrodrigo31@gmail.com>
2021-09-15 21:58:19 -0300

    feat: requisito 9

---
+++ challenges/desafio8.js
@@ -0,0 +1,34 @@
+db.air_routes.aggregate(
+  {
+    $match: {
+      airplane: {
+        $in: ["747", "380"],
+      },
+    },
+  },
+  {
+    $lookup: {
+      from: "air_alliances",
+      localField: "airline.name",
+      foreignField: "airlines",
+      as: "result",
+    },
+  },
+  {
+    $unwind: "$result",
+  },
+  {
+    $group: {
+      _id: "$result.name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+);
